f593a1411b277d84a81a8d6ef55d6418265c443f
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-02-11 17:06:36 +0000

    Initial commit of test tree containing python classes and unittests

---
+++ tools/python/frame_receiver/ipc_channel.py
@@ -0,0 +1,49 @@
+import zmq
+
+class IpcChannelException(Exception):
+    
+    def __init__(self, msg, errno=None):
+        self.msg = msg
+        self.errno = errno
+        
+    def __str__(self):
+        return str(self.msg)
+    
+
+class IpcChannel(object):
+    
+    CHANNEL_TYPE_PAIR = zmq.PAIR
+    
+    def __init__(self, channel_type, endpoint=None, context=None):
+        
+        self.context = context or zmq.Context().instance()
+        self.socket  = self.context.socket(channel_type)
+        
+        if endpoint:
+            self.endpoint = endpoint
+        
+    def bind(self, endpoint=None):
+        
+        if endpoint:
+            self.endpoint = endpoint
+            
+        self.socket.bind(self.endpoint)
+        
+    def connect(self, endpoint=None):
+        
+        if endpoint:
+            self.endpoint = endpoint
+            
+        self.socket.connect(self.endpoint)
+    
+    def close(self):
+        
+        self.socket.close()    
+        
+    def send(self, data):
+        
+        self.socket.send(data)
+        
+    def recv(self):
+        
+        return self.socket.recv()
