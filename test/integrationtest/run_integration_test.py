e7a47c7b66e36129ddca941ef3333c0392dde467
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-10-20 14:58:53 +0100

    Improved operation of integration test script, number of frames programmable from prototype

--- test/integrationtest/run_integration_test.py
+++ test/integrationtest/run_integration_test.py
@@ -1,5 +1,6 @@
 #!/bin/env python
 
+import argparse
 import sys
 import subprocess
 import shlex
@@ -10,27 +11,40 @@ import re
 class IntegrationTest(object):
     
     def __init__(self):
+ 
+        parser = argparse.ArgumentParser(prog="IntegrationTest", description="IntegrationTest - run PERCIVAL frame receiver integration test")
         
-        self.num_frames = 100
+        parser.add_argument('--frames', '-n', type=int, default=100,
+                            help='select number of frames to transmit in test')
+        parser.add_argument('--timeout', '-t', type=float, default=5.0,
+                            help="sets timeout for process completion")
+    
+        args = parser.parse_args()
+    
+        self.frames = args.frames
+        self.timeout = args.timeout
     
     def run(self):
         
         rc = 0
         
-        receiver = self.launch_process("bin/frameReceiver --config test_config/fr_test.config --logconfig test_config/log4cxx.config --debug 2 --frames %d" % self.num_frames)
+        receiver = self.launch_process("bin/frameReceiver --config test_config/fr_test.config --logconfig test_config/log4cxx.config --debug 2 --frames %d" % self.frames)
         time.sleep(0.5)
-        processor = self.launch_process("python -m frame_processor --config test_config/fp_test.config --bypass_mode --frames %d" % self.num_frames)
+        processor = self.launch_process("python -m frame_processor --config test_config/fp_test.config --bypass_mode --frames %d" % self.frames)
         time.sleep(0.5)
-        producer = self.launch_process("python -m frame_producer --destaddr 127.0.0.1:8000 --frames %d" % self.num_frames)
+        producer = self.launch_process("python -m frame_producer --destaddr 127.0.0.1:8000 --frames %d" % self.frames)
         
-        rc = rc + self.validate_process_output(producer, "Producer", "%d frames completed" % self.num_frames)
-        rc = rc + self.validate_process_output(receiver, "Receiver", "Specified number of frames \(%d\) received and released, terminating" % self.num_frames)
-        rc = rc + self.validate_process_output(processor, "Processor", "Specified number of frames \(%d\) received, terminating" % self.num_frames)
+        # Wait for producer to run to completion and validate output. Other processes should complete shortly thereafter, so use a short timeout
+        rc = rc + self.validate_process_output(producer, "Producer", "%d frames completed" % self.frames, None)
+        rc = rc + self.validate_process_output(receiver, "Receiver", 
+           "Specified number of frames \(%d\) received and released, terminating" % self.frames, self.timeout)
+        rc = rc + self.validate_process_output(processor, "Processor",
+           "Specified number of frames \(%d\) received, terminating" % self.frames, self.timeout)
         
         if rc == 0:
-            print "Integration test (%d frames) PASSED" % self.num_frames
+            print "Integration test (%d frames) PASSED" % self.frames
         else:
-            print "Integration test (%d frames) FAILED" % self.num_frames
+            print "Integration test (%d frames) FAILED" % self.frames
             
         return rc
     
@@ -44,11 +58,11 @@ class IntegrationTest(object):
         timedout['value'] = True
         process.kill()
     
-    def validate_process_output(self, process, name, expected_in_output=None):
+    def validate_process_output(self, process, name, expected_in_output=None, timeout=None):
         
         rc = 0
         
-        (process_timedout, process_stdout, process_stderr) = self.wait_process_output(process)
+        (process_timedout, process_stdout, process_stderr) = self.wait_process_output(process, timeout)
         
         if process_timedout:
             print "ERROR:", name, "process timed out. Output follows:"
@@ -58,19 +72,25 @@ class IntegrationTest(object):
                   
         if expected_in_output is not None:
             if not re.search(expected_in_output, process_stdout):
-                print "Failed to find expected output \"%s\" for %s process", (expected_in_output, name)
+                print "Failed to find expected output \"%s\" for %s process. Output follows:", (expected_in_output, name)
+                print process_stdout
+                print process_stderr
                 rc = 1
                  
         return rc
     
-    def wait_process_output(self, process, timeout = 5.0):
+    def wait_process_output(self, process, timeout=None):
         
         timedout = { 'value' : False }
-        timer = Timer(timeout, self.kill_process, [process, timedout])
         
-        timer.start()
+        if timeout is not None:
+            timer = Timer(timeout, self.kill_process, [process, timedout])
+            timer.start()
+            
         (process_stdout, process_stderr) = process.communicate()
-        timer.cancel()
+        
+        if timeout is not None:
+            timer.cancel()
 
         return (timedout['value'], process_stdout, process_stderr)
     
