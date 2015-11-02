719f0db0234f8fcf1b7a6afbce538228b112a40a
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2015-11-02 09:44:13 +0000

    Fixed shared buffer operation under MacOSX
    Under OSX, boost::interprocess::shared_object does not use POSIX IPC
    shared memory but emulates it via mem-mapped files under /tmp. This was
    causing the integration tests to fail completely, while the C++ unit
    tests and python nose tests work fine. Added boost_mmap_mode to
    shared_buffer_manager.py (and associated tests), which can be used to
    turn on emulation mode so that Python inter-operates with C++ correctly.

--- tools/python/frame_receiver/testing/test_shared_buffer_manager.py
+++ tools/python/frame_receiver/testing/test_shared_buffer_manager.py
@@ -6,33 +6,56 @@ shared_mem_name = "TestSharedBuffer"
 buffer_size     = 1000
 num_buffers     = 10
 shared_mem_size = buffer_size * num_buffers
+boost_mmap_mode = True
 
 class TestSharedBufferManager:
     
     @classmethod
     def setup_class(cls):
         
-        cls.shared_buffer_manager = SharedBufferManager(shared_mem_name, shared_mem_size, buffer_size)
-        print "Set up class"
+        # Create a shared buffer manager for use in all tests
+        cls.shared_buffer_manager = SharedBufferManager(
+                shared_mem_name, shared_mem_size, 
+                buffer_size, remove_when_deleted=True, boost_mmap_mode=boost_mmap_mode)
         
     @classmethod
     def teardown_class(cls):
         
-        print "Tear down class"
-        
+        pass
+                
     def test_basic_shared_buffer(self):
-        
+
+        # Test the shared buffer manager confoguration is expected        
         assert_equal(self.shared_buffer_manager.get_num_buffers(), num_buffers)
         assert_equal(self.shared_buffer_manager.get_buffer_size(), buffer_size)
         
-        
     def test_existing_manager(self):
         
-        existing_shared_buffer = SharedBufferManager(shared_mem_name)
+        # Map the existing manager
+        existing_shared_buffer = SharedBufferManager(shared_mem_name, boost_mmap_mode=boost_mmap_mode)
+            
+        # Test that the configuration matches the original
         assert_equal(self.shared_buffer_manager.get_manager_id(), existing_shared_buffer.get_manager_id())
         assert_equal(self.shared_buffer_manager.get_num_buffers(), existing_shared_buffer.get_num_buffers())
         assert_equal(self.shared_buffer_manager.get_buffer_size(), existing_shared_buffer.get_buffer_size())
         
+    def test_existing_manager_absent(self):
+        
+        # Attempt to map a shared buffer manager that doesn't already exist
+        absent_manager_name = "AbsentBufferManager"
+        with assert_raises(SharedBufferManagerException) as cm:
+            existing_shared_buffer = SharedBufferManager(absent_manager_name, boost_mmap_mode=boost_mmap_mode)
+        ex = cm.exception
+        assert_regexp_matches(ex.msg, "No shared memory exists with the specified name")
+        
+    def test_existing_manager_already_exists(self):
+        
+        # Attempt to create a shared buffer manager that already exists
+        with assert_raises(SharedBufferManagerException) as cm:
+            clobbered_shared_buffer = SharedBufferManager(shared_mem_name, 
+                                                          100, 100, True, boost_mmap_mode=boost_mmap_mode)
+        ex = cm.exception
+        assert_regexp_matches(ex.msg, "Shared memory with the specified name already exists")
         
     def test_illegal_shared_buffer_index(self):
 
