9d75c47922db04a9aa30ee772bba38d3aae4995f
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2016-01-15 16:20:47 +0000

    filewriter: unittest configured logger

--- tools/filewriter/FileWriterTest.cpp
+++ tools/filewriter/FileWriterTest.cpp
@@ -9,32 +9,33 @@
 #include <iostream>
 
 #include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+#include <log4cxx/simplelayout.h>
 #include <log4cxx/consoleappender.h>
 #include <log4cxx/basicconfigurator.h>
-#include <log4cxx/simplelayout.h>
+using namespace log4cxx;
+using namespace log4cxx::xml;
 
 #include "FileWriter.h"
 #include "framenotifier_data.h"
 
-class FileWriterTestFixture
-{
+class GlobalConfig {
 public:
-    FileWriterTestFixture() :
-        logger(log4cxx::Logger::getLogger("FileWriterUnitTest"))
-    {
-
+    GlobalConfig() {
+        //std::cout << "GlobalConfig constructor" << std::endl;
+        // Create a default simple console appender for log4cxx.
+        consoleAppender = new ConsoleAppender(LayoutPtr(new SimpleLayout()));
+        BasicConfigurator::configure(AppenderPtr(consoleAppender));
+        Logger::getRootLogger()->setLevel(Level::getWarn());
     }
-    log4cxx::LoggerPtr logger;
+    ~GlobalConfig(){
+        //std::cout << "GlobalConfig constructor" << std::endl;
+        //delete consoleAppender;
+    };
+private:
+    ConsoleAppender *consoleAppender;
 };
-BOOST_FIXTURE_TEST_SUITE(FileWriterUnitTest, FileWriterTestFixture);
-
-BOOST_AUTO_TEST_CASE( FileWriterTest )
-{
-    FileWriter fw;
-    BOOST_CHECK_NO_THROW(fw.createFile("/tmp/blah.h5"));
-}
-
-BOOST_AUTO_TEST_SUITE_END(); //FileWriterUnitTest
+BOOST_GLOBAL_FIXTURE(GlobalConfig);
 
 
 BOOST_AUTO_TEST_SUITE(FrameUnitTest);
@@ -48,10 +49,32 @@ BOOST_AUTO_TEST_CASE( FrameTest )
     FrameHeader img_header;
     img_header.frame_number = 7;
 
-    Frame frame(100, img_dims);
+    Frame frame(2, img_dims);
     BOOST_REQUIRE_EQUAL(frame.get_data_size(), 24);
     BOOST_CHECK_NO_THROW(frame.copy_header(&img_header));
     BOOST_CHECK_NO_THROW(frame.copy_data(static_cast<void*>(img), 24));
+    BOOST_CHECK_EQUAL(frame.get_frame_number(), 7);
+
 }
 
 BOOST_AUTO_TEST_SUITE_END(); //FrameUnitTest
+
+
+class FileWriterTestFixture
+{
+public:
+    FileWriterTestFixture() //: logger(log4cxx::Logger::getLogger("FileWriterUnitTest"))
+    {}
+    ~FileWriterTestFixture(){}
+    log4cxx::LoggerPtr logger;
+};
+BOOST_FIXTURE_TEST_SUITE(FileWriterUnitTest, FileWriterTestFixture);
+
+BOOST_AUTO_TEST_CASE( FileWriterTest )
+{
+    FileWriter fw;
+    BOOST_CHECK_NO_THROW(fw.createFile("/tmp/blah.h5"));
+}
+
+BOOST_AUTO_TEST_SUITE_END(); //FileWriterUnitTest
+
