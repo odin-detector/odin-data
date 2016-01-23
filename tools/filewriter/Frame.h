9695ba1cba5f70e10a2ae49e32351852f06198d0
Ulrik Kofoed Pedersen <ulrikkofoed@gmail.com>
2016-01-23 21:54:59 +0000

    filewriter: adding the frame_info field to FrameHeader
    Necessary after merging the same feature from frameReceiver on master

--- tools/filewriter/Frame.h
+++ tools/filewriter/Frame.h
@@ -49,6 +49,7 @@ static const size_t tail_packet_size    = 512;
 static const size_t num_tail_packets    = 1;
 static const size_t num_subframes       = 2;
 static const size_t num_data_types      = 2;
+static const size_t frame_info_size     = 14;
 
 typedef struct
 {
@@ -56,6 +57,7 @@ typedef struct
     uint32_t frame_state;
     struct timespec frame_start_time;
     uint32_t packets_received;
+    uint8_t  frame_info[frame_info_size];
     uint8_t  packet_state[num_data_types][num_subframes][num_primary_packets + num_tail_packets];
 } FrameHeader;
 
