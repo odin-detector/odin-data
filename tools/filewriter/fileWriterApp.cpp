cdeb63bdf8eaba91af2c3675feaeb264f3d757cf
Alan Greer <alan.greer@diamond.ac.uk>
2016-06-06 16:25:29 +0100

    Updated fileWriterApp to use command line options and to use the cleaned up interface to the file writer controller.

--- tools/filewriter/fileWriterApp.cpp
+++ tools/filewriter/fileWriterApp.cpp
@@ -38,46 +38,169 @@ using namespace rapidjson;
 #include "SharedMemoryParser.h"
 #include "SharedMemoryController.h"
 
+using namespace filewriter;
+
+void parse_arguments(int argc, char** argv, po::variables_map& vm, LoggerPtr& logger)
+{
+    try
+    {
+        std::string config_file;
+
+        // Declare a group of options that will allowed only on the command line
+        po::options_description generic("Generic options");
+        generic.add_options()
+                ("help,h",
+                    "Print this help message")
+                ("config,c",    po::value<string>(&config_file),
+                    "Specify program configuration file")
+                ;
+
+        // Declare a group of options that will be allowed both on the command line
+        // and in the configuration file
+        po::options_description config("Configuration options");
+        config.add_options()
+                ("logconfig,l",  po::value<string>(),
+                    "Set the log4cxx logging configuration file")
+                ("ready",       po::value<std::string>()->default_value("tcp://127.0.0.1:5001"),
+                    "Ready ZMQ endpoint from frameReceiver")
+                ("release",       po::value<std::string>()->default_value("tcp://127.0.0.1:5002"),
+                        "Release frame ZMQ endpoint from frameReceiver")
+                ("frames,f",     po::value<unsigned int>()->default_value(1),
+                    "Set the number of frames to be notified about before terminating")
+                ("sharedbuf",    po::value<std::string>()->default_value("FrameReceiverBuffer"),
+                    "Set the name of the shared memory frame buffer")
+                ("output,o",     po::value<std::string>(),
+                    "Name of HDF5 file to write frames to (default: no file writing)")
+                ("processes,p",  po::value<unsigned int>()->default_value(1),
+                    "Number of concurrent file writer processes"   )
+                ("rank,r",       po::value<unsigned int>()->default_value(0),
+                    "The rank (index number) of the current file writer process in relation to the other concurrent ones")
+                ;
+
+        // Group the variables for parsing at the command line and/or from the configuration file
+        po::options_description cmdline_options;
+        cmdline_options.add(generic).add(config);
+        po::options_description config_file_options;
+        config_file_options.add(config);
+
+        // Parse the command line options
+        po::store(po::parse_command_line(argc, argv, cmdline_options), vm);
+        po::notify(vm);
+
+        // If the command-line help option was given, print help and exit
+        if (vm.count("help"))
+        {
+            std::cout << "usage: fileWriter [options]" << std::endl << std::endl;
+            std::cout << cmdline_options << std::endl;
+            exit(1);
+        }
+
+        // If the command line config option was given, parse the specified configuration
+        // file for additional options. Note that boost::program_options gives precedence
+        // to the first instance occurring. In this case, that implies that command-line
+        // options have precedence over equivalent configuration file entries.
+        if (vm.count("config"))
+        {
+            std::ifstream config_ifs(config_file.c_str());
+            if (config_ifs)
+            {
+                LOG4CXX_DEBUG(logger, "Parsing configuration file " << config_file);
+                po::store(po::parse_config_file(config_ifs, config_file_options, true), vm);
+                po::notify(vm);
+            }
+            else
+            {
+                LOG4CXX_ERROR(logger, "Unable to open configuration file " << config_file << " for parsing");
+                exit(1);
+            }
+        }
+
+        if (vm.count("logconfig"))
+        {
+            PropertyConfigurator::configure(vm["logconfig"].as<string>());
+            LOG4CXX_DEBUG(logger, "log4cxx config file is set to " << vm["logconfig"].as<string>());
+        }
+
+        if (vm.count("ready"))
+        {
+            LOG4CXX_DEBUG(logger, "Setting frame ready notification ZMQ address to " << vm["ready"].as<string>());
+        }
+
+        if (vm.count("release"))
+        {
+            LOG4CXX_DEBUG(logger, "Setting frame release notification ZMQ address to " << vm["release"].as<string>());
+        }
+
+        if (vm.count("frames"))
+        {
+            LOG4CXX_DEBUG(logger, "Setting number of frames to receive to " << vm["frames"].as<unsigned int>());
+        }
+
+        if (vm.count("output"))
+        {
+            LOG4CXX_DEBUG(logger, "Writing frames to file: " << vm["output"].as<string>());
+        }
+
+        if (vm.count("processes"))
+        {
+            LOG4CXX_DEBUG(logger, "Number of concurrent filewriter processes: " << vm["processes"].as<unsigned int>());
+        }
+
+        if (vm.count("rank"))
+        {
+            LOG4CXX_DEBUG(logger, "This process rank (index): " << vm["rank"].as<unsigned int>());
+        }
+
+    }
+    catch (po::unknown_option &e)
+    {
+        LOG4CXX_WARN(logger, "CLI parsing error: " << e.what() << ". Will carry on...");
+    }
+    catch (Exception &e)
+    {
+        LOG4CXX_FATAL(logger, "Got Log4CXX exception: " << e.what());
+        throw;
+    }
+    catch (exception &e)
+    {
+        LOG4CXX_ERROR(logger, "Got exception:" << e.what());
+        throw;
+    }
+    catch (...)
+    {
+        LOG4CXX_FATAL(logger, "Exception of unknown type!");
+        throw;
+    }
+}
 
 int main(int argc, char** argv)
 {
-    int rc = 0;
-
     // Create a default basic logger configuration, which can be overridden by command-line option later
     BasicConfigurator::configure();
 
     LoggerPtr logger(Logger::getLogger("socketApp"));
-//    po::variables_map vm;
-//    parse_arguments(argc, argv, vm, logger);
+    po::variables_map vm;
+    parse_arguments(argc, argv, vm, logger);
 
     boost::shared_ptr<filewriter::FileWriterController> fwc;
     fwc = boost::shared_ptr<filewriter::FileWriterController>(new filewriter::FileWriterController());
-
     filewriter::JSONSubscriber sh("tcp://127.0.0.1:5003");
     sh.registerCallback(fwc);
     sh.subscribe();
 
-    //fwc->loadPlugin("DummyPlugin", "/home/gnx91527/work/percival/framereceiver/build/lib/libDummyPlugin.so");
-//    // Create shared memory parser
-//    boost::shared_ptr<filewriter::SharedMemoryParser> smp;
-//    smp = boost::shared_ptr<filewriter::SharedMemoryParser>(new filewriter::SharedMemoryParser("FrameReceiverBuffer"));
+    // Configure the shared memory setup of the file writer controller
+    std::string cfgString = "{" +
+        std::string("\"fr_release_cnxn\":\"") + vm["release"].as<string>() + std::string("\",") +
+        "\"fr_ready_cnxn\":\"" + vm["ready"].as<string>() + "\"," +
+        "\"fr_shared_mem\":\"" + vm["sharedbuf"].as<string>() + "\"" +
+        "}";
+    cfgString = "{\"fr_setup\": " + cfgString + "}";
+    boost::shared_ptr<JSONMessage> cfg = boost::shared_ptr<JSONMessage>(new JSONMessage(cfgString));
 
-//    // create the zmq publisher
-//    boost::shared_ptr<filewriter::JSONPublisher> frame_release_publisher;
-//    frame_release_publisher = boost::shared_ptr<filewriter::JSONPublisher>(new filewriter::JSONPublisher("tcp://127.0.0.1:5002"));
-//    frame_release_publisher->connect();
+    fwc->configure(cfg);
 
-//    boost::shared_ptr<filewriter::SharedMemoryController> smc;
-//    smc = boost::shared_ptr<filewriter::SharedMemoryController>(new filewriter::SharedMemoryController());
-//    smc->setSharedMemoryParser(smp);
-//    smc->setFrameReleasePublisher(frame_release_publisher);
+    // Now wait for the shutdown
+    fwc->waitForShutdown();
 
-//    filewriter::JSONSubscriber sh2("tcp://127.0.0.1:5001");
-//    sh2.registerCallback(smc);
-//    sh2.subscribe();
-
-
-
-    sleep(60);
     return 0;
 }
