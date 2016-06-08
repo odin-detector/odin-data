8636e80b80376a6ddd36c9b55c7e278eed2e143b
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2016-06-08 15:49:53 +0100

    Fixed .travis-install-hdf5.sh to resolve travis-ci build errors.
    HDF5 library has undergone point release to 1.8.17, so 'current' URL no
    longer points to 1.8.16 version. Changed URL to pull 1.8.16 from
    'releases' path rather than 'current'.

--- .travis-install-hdf5.sh
+++ .travis-install-hdf5.sh
@@ -2,7 +2,7 @@
 set -e
 # check to see if hdf5 folder is empty
 if [ ! -d "$HOME/hdf5-1.8.16/lib" ]; then
-  wget http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.16.tar.bz2;
+  wget https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.16/src/hdf5-1.8.16.tar.bz2;
   tar -jxf hdf5-1.8.16.tar.bz2;
   cd hdf5-1.8.16 && ./configure --prefix=$HOME/hdf5-1.8.16 && make -j && make install;
 else
