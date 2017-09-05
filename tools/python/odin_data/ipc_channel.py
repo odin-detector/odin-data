765875200b5d3cd33ba91d6c1f31f6c38e598a72
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2017-09-05 16:17:08 +0100

    Added prototype DEALER/ROUTER functionality to ipc_channel

--- tools/python/odin_data/ipc_channel.py
+++ tools/python/odin_data/ipc_channel.py
@@ -1,4 +1,7 @@
+import sys
 import zmq
+from zmq.utils.strtypes import unicode, cast_bytes
+import uuid
 
 
 class IpcChannelException(Exception):
@@ -17,33 +20,48 @@ class IpcChannel(object):
     CHANNEL_TYPE_REQ = zmq.REQ
     CHANNEL_TYPE_SUB = zmq.SUB
     CHANNEL_TYPE_PUB = zmq.PUB
+    CHANNEL_TYPE_DEALER = zmq.DEALER
+    CHANNEL_TYPE_ROUTER = zmq.ROUTER
 
-    def __init__(self, channel_type, endpoint=None, context=None):
-        self.context = context or zmq.Context().instance()
-        self.socket = self.context.socket(channel_type)
-
+    def __init__(self, channel_type, endpoint=None, context=None, identity=None):
+        self.channel_type = channel_type
         if endpoint:
             self.endpoint = endpoint
+        self.context = context or zmq.Context().instance()
+        self.socket = self.context.socket(self.channel_type)
+        if self.channel_type == self.CHANNEL_TYPE_DEALER:
+            if identity is None:
+                identity = str(uuid.uuid4())
+            self.socket.setsockopt(zmq.IDENTITY, cast_bytes(identity))
 
     def bind(self, endpoint=None):
         if endpoint:
             self.endpoint = endpoint
+
         self.socket.bind(self.endpoint)
 
     def connect(self, endpoint=None):
         if endpoint:
             self.endpoint = endpoint
+
         self.socket.connect(self.endpoint)
 
     def close(self):
         self.socket.close()
 
     def send(self, data):
-        self.socket.send_string(data)
+        if isinstance(data, unicode):
+            data = cast_bytes(data)
+        self.socket.send(data)
 
     def recv(self):
-        data = self.socket.recv()
-        return data
+        data = list(map(self._cast_str, self.socket.recv_multipart()))
+
+        if self.channel_type == self.CHANNEL_TYPE_ROUTER:
+            identity = data.pop(0)
+            return (identity, data)
+        else:
+            return data[0]
 
     def poll(self, timeout=None):
         pollevts = self.socket.poll(timeout)
@@ -51,3 +69,14 @@ class IpcChannel(object):
 
     def subscribe(self, topic=b''):
         self.socket.setsockopt(zmq.SUBSCRIBE, topic)
+
+    def _cast_str(self, s, encoding='utf8', errors='strict'):
+        """Cast bytes or unicode to unicode for Python 3 strings."""
+        if sys.version_info[0] < 3:
+            return s
+        elif isinstance(s, bytes):
+            return s.decode(encoding, errors)
+        elif isinstance(s, unicode):
+            return s
+        else:
+            raise IpcChannelException("Expected unicode or bytes, got %r" % s)
--- tools/python/odin_data/testing/test_ipc_channel.py
+++ tools/python/odin_data/testing/test_ipc_channel.py
@@ -25,4 +25,28 @@ class TestIpcChannel:
         self.send_channel.send(msg)
 
         reply = self.recv_channel.recv()
+
         assert_equal(msg, reply)
+        assert_equal(type(msg), type(reply))
+
+
+    def test_dealer_router(self):
+
+        endpoint = 'inproc://dr_channel'
+        msg = "This is a dealer router message"
+        dealer_indentity = 'test_dealer'
+
+        dealer_channel = IpcChannel(IpcChannel.CHANNEL_TYPE_DEALER,
+                                    identity=dealer_indentity)
+        router_channel = IpcChannel(IpcChannel.CHANNEL_TYPE_ROUTER)
+
+        router_channel.bind(endpoint)
+        dealer_channel.connect(endpoint)
+
+        dealer_channel.send(msg)
+        (recv_identity, reply) = router_channel.recv()
+
+        assert_equal(len(reply), 1)
+        assert_equal(msg, reply[0])
+        assert_equal(type(msg), type(reply[0]))
+        assert_equal(dealer_indentity, recv_identity)
