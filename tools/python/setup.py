37b6257d64299c827f2596bc1a6abee043263378
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2017-09-05 15:55:27 +0100

    Updated package infrastructure for odin_data

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup
+"""Setup script for odin_data python package."""
 
-setup(name='odin-data',
-      version='0.1',
+import sys
+from setuptools import setup, find_packages
+import versioneer
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+if sys.version_info[0] == 2:
+    required.append('futures>=3.0.0')
+
+setup(name='odin_data',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       description='PERCIVAL Detector Frame Receiver',
-      url='https//github.com/percival-detector/odin-data',
+      url='https//github.com/odin-detector/odin-data',
       author='Tim Nicholls',
       author_email='tim.nicholls@stfc.ac.uk',
-      packages=[
-        'emulator_client',
-        'frame_processor',
-        'frame_producer',
-        'frame_receiver_client',
-        'port_counters',
-        'tools'        
-      ],
-      install_requires=[
-        'pyzmq>=14.0.0',
-        'posix_ipc>=1.0.0',
-        'numpy>=1.9.1',
-        'h5py>=2.5.0'
-      ],
+      packages=find_packages(),
+      install_requires=required,
       entry_points={
-        'console_scripts' : [
+        'console_scripts': [
             'emulator_client = emulator_client.emulator_client:main',
             'frame_processor = frame_processor.frame_processor:main',
             'frame_producer = frame_producer.frame_producer:main',
