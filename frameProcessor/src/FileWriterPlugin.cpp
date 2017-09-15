d3966c61e9f8ebdeaeb32d395db2191ced3beca1
Matthew Taylor <matthew.taylor@diamond.ac.uk>
2017-09-15 10:37:21 +0100

    Checked FileWriterPlugin is expecting frames before starting writing
    BC-499

--- frameProcessor/src/FileWriterPlugin.cpp
+++ frameProcessor/src/FileWriterPlugin.cpp
@@ -739,7 +739,13 @@ void FileWriterPlugin::configure(OdinData::IpcMessage& config, OdinData::IpcMess
   // Final check is to start or stop writing
   if (config.has_param(FileWriterPlugin::CONFIG_WRITE)) {
     if (config.get_param<bool>(FileWriterPlugin::CONFIG_WRITE) == true) {
-      this->startWriting();
+      if (nextAcquisition_.totalFrames_ > 0 && nextAcquisition_.framesToWrite_ == 0) {
+    	// We're not expecting any frames, so just clear out the nextAcquisition for the next one and don't start writing
+    	this->nextAcquisition_ = Acquisition();
+    	LOG4CXX_INFO(logger_, "Not expecting any frames so not starting new acquisition writing");
+      } else {
+        this->startWriting();
+      }
     } else {
       this->stopWriting();
     }
