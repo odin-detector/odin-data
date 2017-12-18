5ad213803524a431d7520f25122f40de5266a58b
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-12-18 17:19:53 +0000

    Update builder to store configuration information

--- etc/builder.py
+++ etc/builder.py
@@ -1,11 +1,24 @@
 from iocbuilder import Device
+from iocbuilder.arginfo import makeArgInfo, Simple
 
 __all__ = ['OdinData']
 
 
 class OdinData(Device):
 
-    """Simply adds ODINDATA path to configure/RELEASE."""
+    """Store configuration for OdinData."""
 
     # Device attributes
     AutoInstantiate = True
+
+    def __init__(self, MACRO, IP, READY, RELEASE, META):
+        self.__super.__init__()
+        # Update attributes with parameters
+        self.__dict__.update(locals())
+
+    ArgInfo = makeArgInfo(__init__,
+                          MACRO=Simple('Dependency MACRO as in configure/RELEASE', str),
+                          IP=Simple('IP address of server hosting preocesses', str),
+                          READY=Simple('Port for Ready Channel', int),
+                          RELEASE=Simple('Port for Release Channel', int),
+                          META=Simple('Port for Meta Channel', int))
