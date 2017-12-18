7594b09f02f959eba3bc1463aca423b65c63e754
Gary Yendell <gary.yendell@diamond.ac.uk>
2017-12-18 13:13:45 +0000

    Add builder support

---
+++ etc/builder.py
@@ -0,0 +1,11 @@
+from iocbuilder import Device
+
+__all__ = ['OdinData']
+
+
+class OdinData(Device):
+
+    """Simply adds ODINDATA path to configure/RELEASE."""
+
+    # Device attributes
+    AutoInstantiate = True
