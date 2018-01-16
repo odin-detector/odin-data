50a68b72aee6de6d17ecd28c937b86e30c3b58e0
Alan Greer <alan.greer@diamond.ac.uk>
2018-01-16 14:34:28 +0000

    Added spaces around the "+" symbol as requested in PR #83.

--- tools/python/odin_data/odin_data_adapter.py
+++ tools/python/odin_data/odin_data_adapter.py
@@ -170,7 +170,7 @@ class OdinDataAdapter(ApiAdapter):
                 index = int(uri_items[-1])
                 if index >= 0:
                     # This is a valid index so remove the value from the URI
-                    request_command = remove_suffix(request_command, "/"+uri_items[-1])
+                    request_command = remove_suffix(request_command, "/" + uri_items[-1])
                     # Set the client index for submitting config to
                     client_index = index
                     logging.debug("URI without index: %s", request_command)
