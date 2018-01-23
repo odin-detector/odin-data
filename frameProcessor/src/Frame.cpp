9f73fcdf110b24346631c29bd5cbe47933dfc927
Ulrik Kofoed Pedersen <ulrik.pedersen@diamond.ac.uk>
2018-01-23 00:29:53 +0000

    Frame class: adding get_data_type_size convenience function

--- frameProcessor/src/Frame.cpp
+++ frameProcessor/src/Frame.cpp
@@ -279,7 +279,7 @@ void Frame::set_parameter(const std::string& index, float value)
 
 /** Set the compression type of the raw data.
  *
- * 0: None, 1: LZ4, 2: BSLZ4
+ * 0: None, 1: LZ4, 2: BSLZ4, 3: Blosc
  *
  * \param compression type.
  */
@@ -297,6 +297,16 @@ void Frame::set_compression(int compression)
 void Frame::set_data_type(int data_type)
 {
   data_type_ = data_type;
+}
+
+size_t Frame::get_data_type_size()
+{
+  int dt = this->get_data_type();
+  if (dt == -1) return 0;
+  else if (dt == 0) return 1;
+  else if (dt == 1) return 2;
+  else if (dt == 2) return 4;
+  else return 0;
 }
 
 /** Return a parameter for this Frame.
