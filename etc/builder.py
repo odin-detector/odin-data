0fb0e64b0c4d161d6b4a08a19c19177dcc4cfa18
Gary Yendell <gary.yendell@diamond.ac.uk>
2018-02-06 14:14:56 +0000

    DLS: Move OdinData builder object to ADOdin and add FileWriterPlugin object

--- etc/builder.py
+++ etc/builder.py
@@ -1,24 +1,19 @@
 from iocbuilder import Device
 from iocbuilder.arginfo import makeArgInfo, Simple
 
-__all__ = ['OdinData']
+__all__ = ['FileWriterPlugin']
 
 
-class OdinData(Device):
+class FileWriterPlugin(Device):
 
-    """Store configuration for OdinData."""
+    """Store configuration for FileWriterPlugin."""
 
     # Device attributes
     AutoInstantiate = True
 
-    def __init__(self, MACRO, IP, READY, RELEASE, META):
+    def __init__(self, MACRO):
         self.__super.__init__()
         # Update attributes with parameters
         self.__dict__.update(locals())
 
-    ArgInfo = makeArgInfo(__init__,
-                          MACRO=Simple('Dependency MACRO as in configure/RELEASE', str),
-                          IP=Simple('IP address of server hosting preocesses', str),
-                          READY=Simple('Port for Ready Channel', int),
-                          RELEASE=Simple('Port for Release Channel', int),
-                          META=Simple('Port for Meta Channel', int))
+    ArgInfo = makeArgInfo(__init__, MACRO=Simple("Dependency MACRO as in configure/RELEASE", str))
