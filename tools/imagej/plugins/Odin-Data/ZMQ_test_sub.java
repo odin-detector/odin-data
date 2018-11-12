8df7c06f1cf4ec9a6a7517bac852d974f33b39cd
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-11-12 10:31:53 +0000

    Added proper shutdown of sockets to the practice ZMQ java files

--- tools/imagej/plugins/Odin-Data/ZMQ_test_sub.java
+++ tools/imagej/plugins/Odin-Data/ZMQ_test_sub.java
@@ -3,11 +3,43 @@ import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
 class subscriber{
-    public static void main (String[] args) {
 
+    private static Context context = ZMQ.context(1);
+    private static ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+
+
+    public static void run_socket()
+    {
+        while(!Thread.currentThread().isInterrupted())
+        {
+            ZMQ.Poller items = new ZMQ.Poller(1);
+            items.register(subscriber, ZMQ.Poller.POLLIN);
+
+            if(items.poll(0) == -1)
+            {
+                System.out.println("POLL RETURN -1");
+                break;
+            }
+
+            if(items.pollin(0))
+            {
+                byte[] msg = subscriber.recv(ZMQ.NOBLOCK);
+
+                String message = new String(msg);
+                System.out.println("RECIVED MESSAGE:");
+                System.out.println("'" + message.toString() + "'");
+            }
+        }
         
-        Context context = ZMQ.context(1);
-        ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
+        System.out.println("THREAD INTERRUPTED");
+        subscriber.close();
+        context.term();
+    }
+
+    public static void main (String[] args) {
+        
+        // Context context = ZMQ.context(1);
+        // ZMQ.Socket subscriber = context.socket(ZMQ.SUB);
         String socket_addr = "tcp://127.0.0.1:5020";
         if(args.length != 0)
         {
@@ -18,13 +50,32 @@ class subscriber{
 
         System.out.println(String.format("Subscribed to addr %s", socket_addr));
 
-        while(true)
+        Thread zmqThread = new Thread()
         {
-            String message = new String(subscriber.recv(0));
-            System.out.println("RECIVED MESSAGE:");
-            System.out.println("'" + message.toString() + "'");
-        }
+            @Override
+            public void run()
+            {
+                run_socket();
+            }
+        };
 
-        
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            @Override
+            public void run(){
+                System.out.println("W: Interrupt received, killing server…");
+                
+                try {
+                    zmqThread.interrupt();
+                    
+                    zmqThread.join();
+                } catch (Exception e) 
+                {
+                    System.out.println("INTERRUPT ERROR: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        zmqThread.start();
     }
 }
