8b293e0f1f99461d4b7406f176fe87407532776b
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-04 11:48:55 +0000

    Created build script to automatically build the imagej plugins and create a script to run imagej with env variables setup

---
+++ tools/imagej/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PROPERTY_FILE=build.properties
+function getProperty
+{
+    PROP_KEY=$1
+    PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+    echo $PROP_VALUE
+}
+
+function build_imagej
+{
+    ant -version
+    echo "Reading Properties"
+    LD_LIBRARY_PATH=$(getProperty "lib.zmq")
+    SOURCE_IJ=$(getProperty "source.ij")
+    SOURCE_PLUGINS=$(getProperty "source.plugins")
+    echo "Cleaning worksplace..."
+    ant clean
+    echo "Building Plugin..."
+    ant build
+
+    echo "Creating odin-imagej startup script"
+
+    echo "#!/bin/bash
+    echo 'Running ImageJ'
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH
+    java -Xmx32000m -jar $SOURCE_IJ -ijpath $SOURCE_PLUGINS -port0 $@" > odin-imagej
+}
+
+build_imagej
