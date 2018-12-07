fef6b6438e46340fb46047049c79ab4907bd8b81
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-07 13:37:36 +0000

    Modified build script so that it can modify the properties file if given command line arguments pointing at the required libraries

--- tools/imagej/build.sh
+++ tools/imagej/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROPERTY_FILE=build.properties
+
 function getProperty
 {
     PROP_KEY=$1
@@ -8,13 +9,17 @@ function getProperty
     echo $PROP_VALUE
 }
 
-function build_imagej
+function setProperty
+{
+    PROP_KEY=$1
+    PROP_VAL=$2
+    sed -i -r "s:($PROP_KEY[ \t]*=)[ \t]*(.*):\1 $PROP_VAL:" $PROPERTY_FILE
+    echo getProperty $PROP_KEY
+}
+
+function buildImagej
 {
     ant -version
-    echo "Reading Properties"
-    LD_LIBRARY_PATH=$(getProperty "lib.zmq")
-    SOURCE_IJ=$(getProperty "source.ij")
-    SOURCE_PLUGINS=$(getProperty "source.plugins")
     echo "Cleaning worksplace..."
     ant clean
     echo "Building Plugin..."
@@ -27,4 +32,51 @@ function build_imagej
     java -Djava.library.path=$LD_LIBRARY_PATH -Xmx32000m -jar $SOURCE_IJ -ijpath $SOURCE_PLUGINS -port0 $@" > odin-imagej
 }
 
-build_imagej
+function setAllProperties
+{
+    # get the arguments from the command line
+    while getopts ":l:i:p:h" opt; do
+        case $opt in
+            l) LD_LIBRARY_PATH=$OPTARG
+            echo "LD_LIBRARY_PATH SET to $OPTARG"
+            ;;
+            i) SOURCE_IJ="$OPTARG"
+            echo "SOURCE_IJ SET TO $OPTARG"
+            ;;
+            p) SOURCE_PLUGINS="$OPTARG"
+            echo "SOURCE_PLUGINS SET TO $OPTARG"
+            ;;
+            h) echo "HELP"
+            ;;
+            \?) echo "INVALID ARGUMENT -$OPTARG"
+            ;;
+        esac
+    done
+
+    if [ -z $LD_LIBRARY_PATH]
+    then
+        echo "LD_LIBRARY_PATH UNSET. READING FROM FILE"
+        LD_LIBRARY_PATH=$(getProperty "lib.zmq")
+    else
+        setProperty "lib.zmq" "$LD_LIBRARY_PATH"
+    fi
+
+    if [ -z $SOURCE_IJ]
+    then
+        echo "SOURCE_IJ UNSET. READING FROM FILE"
+        SOURCE_IJ=$(getProperty "source.ij")
+    else
+        setProperty "source.ij" "$SOURCE_IJ"
+    fi
+
+    if [ -z $SOURCE_PLUGINS]
+    then
+        echo "SOURCE_PLUGINS UNSET. READING FROM FILE"
+        SOURCE_PLUGINS=$(getProperty "source.plugins")
+    else
+        setProperty "source.plugins" "$SOURCE_PLUGINS"
+    fi
+}
+
+setAllProperties "$@"
+buildImagej
