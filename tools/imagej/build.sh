99a71c1f5719e750d50a990ad677272983b1bd55
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-11 15:00:11 +0000

    Build script now looks for source in multiple places if not provided as a command line argument

--- tools/imagej/build.sh
+++ tools/imagej/build.sh
@@ -12,6 +12,11 @@ OPTIONAL ARGUMENTS:
   -j: Path to the json jar library.
   -h: Display this help text"
 
+ZMQ_LIB_STANDARD="/aeg_sw/tools/CentOS7-x86_64/zeromq/4-2-3/prefix/lib/"
+ZMQ_JAR_STANDARD="lib/zmq.jar"
+JZON_JAR_STANDARD="lib/json.jar"
+IJ_JAR_STANDARD="/aeg_sw/apps/imagej/1.52/ImageJ/ij.jar"
+IJ_PLUGIN_STANDARD="plugins/"
 function getProperty
 {
     PROP_KEY=$1
@@ -33,16 +38,14 @@ function buildImagej
     ant -version
     echo "Cleaning worksplace..."
     ant clean
-    echo "Building Plugin..."
-    ant build
-    echo "Installing Plugin..."
+    echo "Building and Installing Plugin..."
     ant install
 
     echo "Creating odin-imagej startup script"
 
     echo "#!/bin/bash
     echo 'Running ImageJ for Odin'
-    java -Djava.library.path=$LD_LIBRARY_PATH -Xmx32000m -jar $SOURCE_IJ -ijpath $SOURCE_PLUGINS" > odin-imagej
+    java -Djava.library.path=$ZMQ_LIBRARY_PATH -Xmx32000m -jar $SOURCE_IJ -ijpath $SOURCE_PLUGINS" > odin-imagej
 }
 
 function setAllProperties
@@ -50,8 +53,8 @@ function setAllProperties
     # get the arguments from the command line
     while getopts ":l:i:p:z:j:h" opt; do
         case $opt in
-            l) LD_LIBRARY_PATH=$OPTARG
-            echo "LD_LIBRARY_PATH SET to $OPTARG"
+            l) ZMQ_LIBRARY_PATH=$OPTARG
+            echo "ZMQ_LIBRARY_PATH SET to $OPTARG"
             ;;
             i) SOURCE_IJ="$OPTARG"
             echo "SOURCE_IJ SET TO $OPTARG"
@@ -73,42 +76,88 @@ function setAllProperties
         esac
     done
 
-    if [ -z $LD_LIBRARY_PATH ]
+    if [ -f "$ZMQ_LIBRARY_PATH/libjzmq.so" ] && [ ! -z $ZMQ_LIBRARY_PATH ]
     then
-        echo "LD_LIBRARY_PATH UNSET. READING FROM FILE"
-        LD_LIBRARY_PATH=$(getProperty "lib.zmq")
+        # set from command line
+        setProperty "lib.zmq" "$ZMQ_LIBRARY_PATH"
+    elif [ -f "$(getProperty 'lib.zmq')/libjzmq.so" ]
+    then
+        # set from properties file
+        echo "READING ZMQ_LIBRARY_PATH FROM FILE"
+        ZMQ_LIBRARY_PATH=$(getProperty "lib.zmq")
+    elif [ -f "$ZMQ_LIB_STANDARD/libjzmq.so" ]
+    then
+        # standard library location
+        echo "USING STANDARD ZMQ LIBRARY LOCATION"
+        setProperty "lib.zmq" $ZMQ_LIB_STANDARD
+        ZMQ_LIBRARY_PATH=$ZMQ_LIB_STANDARD
     else
-        setProperty "lib.zmq" "$LD_LIBRARY_PATH"
+        echo "ZMQ LIBRARIES NOT FOUND"
+        exit -1
     fi
 
-    if [ -z $SOURCE_IJ ]
+    if [ -f $SOURCE_IJ ] && [ ! -z $SOURCE_IJ ]
     then
+        setProperty "source.ij" "$SOURCE_IJ"
+    elif [ -f $(getProperty "source.ij") ]   
+    then 
         echo "SOURCE_IJ UNSET. READING FROM FILE"
         SOURCE_IJ=$(getProperty "source.ij")
+    elif [ -f $IJ_JAR_STANDARD ]
+    then
+        echo "USING STANDARD IMAGEJ LOCATION"
+        SOURCE_IJ=$IJ_JAR_STANDARD
     else
-        setProperty "source.ij" "$SOURCE_IJ"
+        echo "IMAGEJ NOT FOUND"
+        exit -1
     fi
 
-    if [ -z $SOURCE_PLUGINS ]
+    if [ -d $SOURCE_PLUGINS ] && [ ! -z $SOURCE_PLUGINS ]
+    then
+        setProperty "source.plugins" "$SOURCE_PLUGINS"
+    elif [ -d $(getProperty "source.plugins") ]
     then
         echo "SOURCE_PLUGINS UNSET. READING FROM FILE"
         SOURCE_PLUGINS=$(getProperty "source.plugins")
+    elif [ -d $IJ_PLUGIN_STANDARD ]
+    then
+        echo "USING STANDARD PLUGIN LOCATION"
+        SOURCE_PLUGINS=$IJ_PLUGIN_STANDARD
     else
-        setProperty "source.plugins" "$SOURCE_PLUGINS"
+        echo "NO PLUGIN FOLDER FOUND"
+        exit -1
     fi
 
-    if [ -z $SOURCE_ZMQ ]
+    if [ -f $SOURCE_ZMQ ] && [ ! -z $SOURCE_ZMQ ]
     then
-        echo "ZMQ SOURCE NOT SET"
-    else
         setProperty "source.zmq" "$SOURCE_ZMQ"
+    elif [ -f $(getProperty "source.zmq") ]    
+    then
+        echo "SOURCE_ZMQ UNSET. READING FROM FILE"
+        SOURCE_ZMQ=$(getProperty "source.zmq")
+    elif [ -f $ZMQ_JAR_STANDARD]
+    then
+        echo "USING STANDARD IMAGEJ LOCATION"
+        SOURCE_ZMQ=$ZMQ_JAR_STANDARD
+    else
+        echo "ZMQ JAR NOT FOUND"
+        exit -1
     fi
 
-    if [ -z $SOURCE_JSON ]
+    if [ -f $SOURCE_JSON ] && [ ! -z $SOURCE_JSON ]
     then
-        echo "JSON SOURCE NOT SET"
-    else
         setProperty "source.json" "$SOURCE_JSON"
+    elif [ -f $(getProperty "source.json") ]
+    then
+        echo "SOURCE_JSON UNSET. READING FROM FILE"
+        SOURCE_JSON=$(getProperty "source.json")
+    elif [ -f $JSON_JAR_STANDARD ]
+    then
+        echo "USING STANDARD IMAGEJ LOCATION"
+        SOURCE_JSON=$JSON_JAR_STANDARD
+    else
+        echo "JSON JAR NOT FOUND"
+        exit -1
     fi
 }
 
