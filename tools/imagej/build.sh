edcc6b28e5e6fca5282d17d49d33c19d6f34fd09
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-13 14:37:21 +0000

    can now specify install prefix using buils script

--- tools/imagej/build.sh
+++ tools/imagej/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-PROPERTY_FILE=build.properties
+PROPERTY_FILE=build/build.properties
+ZMQ_LIB_STANDARD=~/develop/Java/jzmq-install/lib
+ZMQ_JAR_STANDARD=lib/zmq.jar
+JSON_JAR_STANDARD=lib/json.jar
+IJ_JAR_STANDARD=/aeg_sw/apps/imagej/1.52/ImageJ/ij.jar
+IJ_PLUGIN_STANDARD=plugins
+INSTALL_PREFIX_STANDARD=~/develop/projects/odin_data/install
+
 HELP_TEXT="HELP
 This build file will compile the ImageJ Live View plugin, and then create a script
 to run it with environmental variables set up automatically.
@@ -10,13 +17,9 @@ OPTIONAL ARGUMENTS:
   -p: Path to the directory containing the ImageJ plugins.
   -z: Path to the zmq jar library.
   -j: Path to the json jar library.
+  -x: Path to the installation directory.
   -h: Display this help text"
-
-ZMQ_LIB_STANDARD="../../../../../Java/jzmq-install/lib"
-ZMQ_JAR_STANDARD="lib/zmq.jar"
-JSON_JAR_STANDARD="lib/json.jar"
-IJ_JAR_STANDARD="/aeg_sw/apps/imagej/1.52/ImageJ/ij.jar"
-IJ_PLUGIN_STANDARD="plugins/"
+  
 function getProperty
 {
     PROP_KEY=$1
@@ -28,7 +31,16 @@ function setProperty
 {
     PROP_KEY=$1
     PROP_VAL=$2
-    sed -i -r "s:($PROP_KEY[ \t]*=)[ \t]*(.*):\1 $PROP_VAL:" $PROPERTY_FILE
+    # if the properties file contains a property with this name
+    if grep -q "$PROP_KEY\s*=" $PROPERTY_FILE
+    then
+        # replace what follows after the = sign with the new value
+        sed -i -r "s:($PROP_KEY\s*=)\s*(.*):\1 $PROP_VAL:" $PROPERTY_FILE
+    else
+        # add key value string to properties file
+        echo "$PROP_KEY = $PROP_VAL" >> $PROPERTY_FILE
+    fi
+
     getProperty $PROP_KEY
 }
 
@@ -45,13 +57,13 @@ function buildImagej
 
     echo "#!/bin/bash
     echo 'Running ImageJ for Odin'
-    java -Djava.library.path=$ZMQ_LIBRARY_PATH -Xmx32000m -jar $SOURCE_IJ -ijpath $SOURCE_PLUGINS" > odin-imagej
+    java -Djava.library.path=$ZMQ_LIBRARY_PATH -Xmx32000m -jar $INSTALL_PREFIX/bin/ij.jar -ijpath $INSTALL_PREFIX/lib/plugins" > odin-imagej
 }
 
 function setAllProperties
 {
     # get the arguments from the command line
-    while getopts ":l:i:p:z:j:h" opt; do
+    while getopts ":l:i:p:z:j:x:h" opt; do
         case $opt in
             l) ZMQ_LIBRARY_PATH=$OPTARG
             echo "ZMQ_LIBRARY_PATH SET to $OPTARG"
@@ -68,6 +80,9 @@ function setAllProperties
             j) SOURCE_JSON="$OPTARG"
             echo "SOURCE_JSON SET TO $OPTARG"
             ;;
+            x) INSTALL_PREFIX="$OPTARG"
+            echo "INSTALL_PREFIX SET TO $OPTARG"
+            ;;
             h) echo "$HELP_TEXT"
             exit 0
             ;;
@@ -80,12 +95,12 @@ function setAllProperties
     then
         # set from command line
         setProperty "lib.zmq" "$ZMQ_LIBRARY_PATH"
-    elif [ -f "$(getProperty 'lib.zmq')/libjzmq.so" ]
+    elif [ -f "$(getProperty 'lib.zmq')/libjzmq.so" ] && [ ! -z  $(getProperty 'lib.zmq') ]
     then
         # set from properties file
         echo "READING ZMQ_LIBRARY_PATH FROM FILE"
         ZMQ_LIBRARY_PATH=$(getProperty "lib.zmq")
-    elif [ -f "$ZMQ_LIB_STANDARD/libjzmq.so" ]
+    elif [ -f  "$ZMQ_LIB_STANDARD/libjzmq.so" ]
     then
         # standard library location
         echo "USING STANDARD ZMQ LIBRARY LOCATION"
@@ -99,7 +114,7 @@ function setAllProperties
     if [ -f $SOURCE_IJ ] && [ ! -z $SOURCE_IJ ]
     then
         setProperty "source.ij" "$SOURCE_IJ"
-    elif [ -f $(getProperty "source.ij") ]   
+    elif [ -f $(getProperty "source.ij") ] && [ ! -z $(getProperty 'source.ij') ] 
     then 
         echo "SOURCE_IJ UNSET. READING FROM FILE"
         SOURCE_IJ=$(getProperty "source.ij")
@@ -116,7 +131,7 @@ function setAllProperties
     if [ -d $SOURCE_PLUGINS ] && [ ! -z $SOURCE_PLUGINS ]
     then
         setProperty "source.plugins" "$SOURCE_PLUGINS"
-    elif [ -d $(getProperty "source.plugins") ]
+    elif [ -d $(getProperty "source.plugins") ] && [ ! -z $(getProperty 'source.plugins') ] 
     then
         echo "SOURCE_PLUGINS UNSET. READING FROM FILE"
         SOURCE_PLUGINS=$(getProperty "source.plugins")
@@ -133,7 +148,7 @@ function setAllProperties
     if [ -f $SOURCE_ZMQ ] && [ ! -z $SOURCE_ZMQ ]
     then
         setProperty "source.zmq" "$SOURCE_ZMQ"
-    elif [ -f $(getProperty "source.zmq") ]    
+    elif [ -f $(getProperty "source.zmq") ] && [ ! -z $(getProperty 'source.zmq') ]
     then
         echo "SOURCE_ZMQ UNSET. READING FROM FILE"
         SOURCE_ZMQ=$(getProperty "source.zmq")
@@ -150,7 +165,7 @@ function setAllProperties
     if [ -f $SOURCE_JSON ] && [ ! -z $SOURCE_JSON ]
     then
         setProperty "source.json" "$SOURCE_JSON"
-    elif [ -f $(getProperty "source.json") ]
+    elif [ -f $(getProperty "source.json") ] && [ ! -z $(getProperty 'source.json') ]
     then
         echo "SOURCE_JSON UNSET. READING FROM FILE"
         SOURCE_JSON=$(getProperty "source.json")
@@ -161,6 +176,23 @@ function setAllProperties
         setProperty "source.json" "$SOURCE_JSON"
     else
         echo "JSON JAR NOT FOUND"
+        exit -1
+    fi
+
+    if [ -d $INSTALL_PREFIX ] && [ ! -z $INSTALL_PREFIX ]
+    then
+        setProperty "prefix.install" "$INSTALL_PREFIX"
+    elif [ -d $(getProperty "prefix.install") ] && [ ! -z $(getProperty 'prefix.install') ] 
+    then
+        echo "INSTALL_PREFIX UNSET. READING FROM FILE"
+        INSTALL_PREFIX=$(getProperty "prefix.install")
+    elif [ -d $INSTALL_PREFIX_STANDARD ]
+    then
+        echo "USING STANDARD INSTALL LOCATION"
+        INSTALL_PREFIX=$INSTALL_PREFIX_STANDARD
+        setProperty "prefix.install" "$INSTALL_PREFIX"
+    else
+        echo "NO INSTALL FOLDER FOUND"
         exit -1
     fi
 }
