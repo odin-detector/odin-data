49c0ae20f83dd24631aca413a7f9b4b5ec2a66aa
Adam Neaves <adam.neaves@stfc.ac.uk>
2018-12-14 14:24:51 +0000

    Modified build script so it can now find jar files and libraries required in the install prefix. also, removed warnings about 'build.properties: no such file or directory' if file not yet created. Added instructions on how to use build script.

--- tools/imagej/build.sh
+++ tools/imagej/build.sh
@@ -1,30 +1,27 @@
 #!/bin/bash
 
 PROPERTY_FILE=build/build.properties
-ZMQ_LIB_STANDARD=~/develop/Java/jzmq-install/lib
-ZMQ_JAR_STANDARD=lib/zmq.jar
-JSON_JAR_STANDARD=lib/json.jar
-IJ_JAR_STANDARD=/aeg_sw/apps/imagej/1.52/ImageJ/ij.jar
-IJ_PLUGIN_STANDARD=plugins
-INSTALL_PREFIX_STANDARD=~/develop/projects/odin_data/install
+INSTALL_PREFIX_STANDARD=/usr/local
 
 HELP_TEXT="HELP
 This build file will compile the ImageJ Live View plugin, and then create a script
 to run it with environmental variables set up automatically.
 OPTIONAL ARGUMENTS:
-  -l: Path to the directory containing libjzmq, the zmq java bindings.
-  -i: Path to the ImageJ jar Library.
-  -p: Path to the directory containing the ImageJ plugins.
-  -z: Path to the zmq jar library.
-  -j: Path to the json jar library.
-  -x: Path to the installation directory.
-  -h: Display this help text"
+  -l|--with-zmq-library: Path to the directory containing libjzmq, the zmq java bindings.
+  -i|--with-imagej     : Path to the ImageJ jar Library.
+  -z|--with-zmq        : Path to the zmq jar library.
+  -j|--with-json       : Path to the json jar library.
+  -x|--prefix          : Path to the installation directory.
+  -h|--help            : Display this help text."
   
 function getProperty
 {
-    PROP_KEY=$1
-    PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY\s*=" | cut -d'=' -f2`
-    echo $PROP_VALUE
+    if [ -f $PROPERTY_FILE ]
+    then
+        PROP_KEY=$1
+        PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY\s*=" | cut -d'=' -f2`
+        echo $PROP_VALUE
+    fi
 }
 
 function setProperty
@@ -32,7 +29,7 @@ function setProperty
     PROP_KEY=$1
     PROP_VAL=$2
     # if the properties file contains a property with this name
-    if grep -q "$PROP_KEY\s*=" $PROPERTY_FILE
+    if [ -f $PROPERTY_FILE ] && grep -q "$PROP_KEY\s*=" $PROPERTY_FILE
     then
         # replace what follows after the = sign with the new value
         sed -i -r "s:($PROP_KEY\s*=)\s*(.*):\1 $PROP_VAL:" $PROPERTY_FILE
@@ -40,8 +37,6 @@ function setProperty
         # add key value string to properties file
         echo "$PROP_KEY = $PROP_VAL" >> $PROPERTY_FILE
     fi
-
-    getProperty $PROP_KEY
 }
 
 function buildImagej
@@ -57,39 +52,96 @@ function buildImagej
 
     echo "#!/bin/bash
     echo 'Running ImageJ for Odin'
-    java -Djava.library.path=$ZMQ_LIBRARY_PATH -Xmx32000m -jar $INSTALL_PREFIX/bin/ij.jar -ijpath $INSTALL_PREFIX/lib/plugins" > odin-imagej
+    java -Djava.library.path=$ZMQ_LIBRARY_PATH -Xmx32000m -jar $INSTALL_PREFIX/lib/ij.jar -ijpath $INSTALL_PREFIX/lib/plugins" > $INSTALL_PREFIX/bin/odin-imagej
 }
 
 function setAllProperties
 {
     # get the arguments from the command line
-    while getopts ":l:i:p:z:j:x:h" opt; do
-        case $opt in
-            l) ZMQ_LIBRARY_PATH=$OPTARG
-            echo "ZMQ_LIBRARY_PATH SET to $OPTARG"
-            ;;
-            i) SOURCE_IJ="$OPTARG"
-            echo "SOURCE_IJ SET TO $OPTARG"
-            ;;
-            p) SOURCE_PLUGINS="$OPTARG"
-            echo "SOURCE_PLUGINS SET TO $OPTARG"
-            ;;
-            z) SOURCE_ZMQ="$OPTARG"
-            echo "SOURCE_ZMQ SET TO $OPTARG"
-            ;;
-            j) SOURCE_JSON="$OPTARG"
-            echo "SOURCE_JSON SET TO $OPTARG"
-            ;;
-            x) INSTALL_PREFIX="$OPTARG"
-            echo "INSTALL_PREFIX SET TO $OPTARG"
-            ;;
-            h) echo "$HELP_TEXT"
+    while :; do
+        case $1 in
+            -h|--help) 
+            echo "$HELP_TEXT"
             exit 0
             ;;
-            \?) echo "INVALID ARGUMENT -$OPTARG"
+            -l|--with-zmq-library)
+            ZMQ_LIBRARY_PATH=$2
+            shift
             ;;
+            -i|--with-imagej)
+            SOURCE_IJ=$2
+            shift
+            ;;
+            -z|--with-zmq)
+            SOURCE_ZMQ=$2
+            shift
+            ;;
+            -j|--with-json)
+            SOURCE_JSON=$2
+            shift
+            ;;
+            -x|--prefix)
+            INSTALL_PREFIX=$2
+            shift
+            ;;
+            *) break
         esac
+        
+        shift
     done
+
+    if [ -f $SOURCE_IJ ] && [ ! -z $SOURCE_IJ ]
+    then
+        setProperty "source.ij" "$SOURCE_IJ"
+    elif [ -f $(getProperty "source.ij") ] && [ ! -z $(getProperty 'source.ij') ] 
+    then 
+        SOURCE_IJ=$(getProperty "source.ij")
+    else
+        die "Error: ImageJ not found. Ensure it is installed and provide the path to 'ij.jar' using '--with-imagej'"
+    fi
+
+    if [ -d $INSTALL_PREFIX ] && [ ! -z $INSTALL_PREFIX ]
+    then
+        setProperty "prefix.install" "$INSTALL_PREFIX"
+    elif [ -d $(getProperty "prefix.install") ] && [ ! -z $(getProperty 'prefix.install') ] 
+    then
+        INSTALL_PREFIX=$(getProperty "prefix.install")
+    elif [ -d $INSTALL_PREFIX_STANDARD ]
+    then
+        INSTALL_PREFIX=$INSTALL_PREFIX_STANDARD
+        setProperty "prefix.install" "$INSTALL_PREFIX"
+        echo "USING STANDARD INSTALL LOCATION: $INSTALL_PREFIX"
+    else
+        die "Error: No valid Install prefix found. Please provide one using '--prefix'."
+    fi
+
+    if [ -f $SOURCE_ZMQ ] && [ ! -z $SOURCE_ZMQ ]
+    then
+        setProperty "source.zmq" "$SOURCE_ZMQ"
+    elif [ -f $(getProperty "source.zmq") ] && [ ! -z $(getProperty 'source.zmq') ]
+    then
+        SOURCE_ZMQ=$(getProperty "source.zmq")
+    elif [ -f $INSTALL_PREFIX/share/java/zmq.jar ]
+    then
+        SOURCE_ZMQ=$INSTALL_PREFIX/share/java/zmq.jar
+        setProperty "source.zmq" "$SOURCE_ZMQ"
+    else
+        die "Error: No zmq.jar found. Please ensure JZMQ has been installed."
+    fi
+
+    if [ -f $SOURCE_JSON ] && [ ! -z $SOURCE_JSON ]
+    then
+        setProperty "source.json" "$SOURCE_JSON"
+    elif [ -f $(getProperty "source.json") ] && [ ! -z $(getProperty 'source.json') ]
+    then
+        SOURCE_JSON=$(getProperty "source.json")
+    elif [ -f $INSTALL_PREFIX/share/java/json.jar ]
+    then
+        SOURCE_JSON=$INSTALL_PREFIX/share/java/json.jar
+        setProperty "source.json" "$SOURCE_JSON"
+    else
+        die "Error: No JSON Libraries found. Please ensure json.jar has been downloaded."
+    fi
 
     if [ -f "$ZMQ_LIBRARY_PATH/libjzmq.so" ] && [ ! -z $ZMQ_LIBRARY_PATH ]
     then
@@ -98,103 +150,20 @@ function setAllProperties
     elif [ -f "$(getProperty 'lib.zmq')/libjzmq.so" ] && [ ! -z  $(getProperty 'lib.zmq') ]
     then
         # set from properties file
-        echo "READING ZMQ_LIBRARY_PATH FROM FILE"
         ZMQ_LIBRARY_PATH=$(getProperty "lib.zmq")
-    elif [ -f  "$ZMQ_LIB_STANDARD/libjzmq.so" ]
+    elif [ -d $INSTALL_PREFIX/lib ]
     then
-        # standard library location
-        echo "USING STANDARD ZMQ LIBRARY LOCATION"
-        ZMQ_LIBRARY_PATH=$ZMQ_LIB_STANDARD
+        ZMQ_LIBRARY_PATH=$INSTALL_PREFIX/lib
         setProperty "lib.zmq" "$ZMQ_LIBRARY_PATH"
     else
-        echo "ZMQ LIBRARIES NOT FOUND"
-        exit -1
+        die "Error: No ZMQ Libraries found. Please ensure JZMQ has been installed."
     fi
+}
 
-    if [ -f $SOURCE_IJ ] && [ ! -z $SOURCE_IJ ]
-    then
-        setProperty "source.ij" "$SOURCE_IJ"
-    elif [ -f $(getProperty "source.ij") ] && [ ! -z $(getProperty 'source.ij') ] 
-    then 
-        echo "SOURCE_IJ UNSET. READING FROM FILE"
-        SOURCE_IJ=$(getProperty "source.ij")
-    elif [ -f $IJ_JAR_STANDARD ]
-    then
-        echo "USING STANDARD IMAGEJ LOCATION"
-        SOURCE_IJ=$IJ_JAR_STANDARD
-        setProperty "source.ij" "$SOURCE_IJ"
-    else
-        echo "IMAGEJ NOT FOUND"
-        exit -1
-    fi
-
-    if [ -d $SOURCE_PLUGINS ] && [ ! -z $SOURCE_PLUGINS ]
-    then
-        setProperty "source.plugins" "$SOURCE_PLUGINS"
-    elif [ -d $(getProperty "source.plugins") ] && [ ! -z $(getProperty 'source.plugins') ] 
-    then
-        echo "SOURCE_PLUGINS UNSET. READING FROM FILE"
-        SOURCE_PLUGINS=$(getProperty "source.plugins")
-    elif [ -d $IJ_PLUGIN_STANDARD ]
-    then
-        echo "USING STANDARD PLUGIN LOCATION"
-        SOURCE_PLUGINS=$IJ_PLUGIN_STANDARD
-        setProperty "source.plugins" "$SOURCE_PLUGINS"
-    else
-        echo "NO PLUGIN FOLDER FOUND"
-        exit -1
-    fi
-
-    if [ -f $SOURCE_ZMQ ] && [ ! -z $SOURCE_ZMQ ]
-    then
-        setProperty "source.zmq" "$SOURCE_ZMQ"
-    elif [ -f $(getProperty "source.zmq") ] && [ ! -z $(getProperty 'source.zmq') ]
-    then
-        echo "SOURCE_ZMQ UNSET. READING FROM FILE"
-        SOURCE_ZMQ=$(getProperty "source.zmq")
-    elif [ -f $ZMQ_JAR_STANDARD ]
-    then
-        echo "USING STANDARD ZMQ LOCATION"
-        SOURCE_ZMQ=$ZMQ_JAR_STANDARD
-        setProperty "source.zmq" "$SOURCE_ZMQ"
-    else
-        echo "ZMQ JAR NOT FOUND"
-        exit -1
-    fi
-
-    if [ -f $SOURCE_JSON ] && [ ! -z $SOURCE_JSON ]
-    then
-        setProperty "source.json" "$SOURCE_JSON"
-    elif [ -f $(getProperty "source.json") ] && [ ! -z $(getProperty 'source.json') ]
-    then
-        echo "SOURCE_JSON UNSET. READING FROM FILE"
-        SOURCE_JSON=$(getProperty "source.json")
-    elif [ -f $JSON_JAR_STANDARD ]
-    then
-        echo "USING STANDARD JSON LOCATION"
-        SOURCE_JSON=$JSON_JAR_STANDARD
-        setProperty "source.json" "$SOURCE_JSON"
-    else
-        echo "JSON JAR NOT FOUND"
-        exit -1
-    fi
-
-    if [ -d $INSTALL_PREFIX ] && [ ! -z $INSTALL_PREFIX ]
-    then
-        setProperty "prefix.install" "$INSTALL_PREFIX"
-    elif [ -d $(getProperty "prefix.install") ] && [ ! -z $(getProperty 'prefix.install') ] 
-    then
-        echo "INSTALL_PREFIX UNSET. READING FROM FILE"
-        INSTALL_PREFIX=$(getProperty "prefix.install")
-    elif [ -d $INSTALL_PREFIX_STANDARD ]
-    then
-        echo "USING STANDARD INSTALL LOCATION"
-        INSTALL_PREFIX=$INSTALL_PREFIX_STANDARD
-        setProperty "prefix.install" "$INSTALL_PREFIX"
-    else
-        echo "NO INSTALL FOLDER FOUND"
-        exit -1
-    fi
+function die
+{
+    printf '%s\n' "$1" >&2
+    exit 1
 }
 
 setAllProperties "$@"
