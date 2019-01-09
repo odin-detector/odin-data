3f9bc24f79a880c54f12964333ad7b909b2e9477
Sky French <sky.french@diamond.ac.uk>
2019-01-09 09:29:46 +0000

    modify travis-install-blosc.sh to install c-blosc-1.13.1 to expected location

--- .travis-install-blosc.sh
+++ .travis-install-blosc.sh
@@ -3,14 +3,13 @@ set -e
 BLOSC=c-blosc-1.13.1
 
 # check to see if blosc folder is empty
-if [ ! -d "$HOME/${BLOSC}/blosc" ]; then
+if [ ! -d "$HOME/${BLOSC}/lib" ]; then
   curl --output ${BLOSC}.tar.gz -L https://codeload.github.com/Blosc/c-blosc/tar.gz/v1.13.1;
   tar -zxf ${BLOSC}.tar.gz;
   cd ${BLOSC};
-  install_prefix=`pwd`
   mkdir build;
   cd build;
-  cmake -DCMAKE_INSTALL_PREFIX=$install_prefix ..;
+  cmake -DCMAKE_INSTALL_PREFIX=$HOME/$BLOSC ..;
   cmake --build . --target install;
 else
   echo 'Using cached directory.';
