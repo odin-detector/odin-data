90cf274f0089358ca5138a07fbbac54b94107793
Adam Neaves <adam.neaves@stfc.ac.uk>
2019-01-31 09:15:49 +0000

    Added basic GET PUT functionality and basic ZMQ connections

--- tools/python/odin_data/live_view_proxy_adapter.py
+++ tools/python/odin_data/live_view_proxy_adapter.py
@@ -35,22 +35,54 @@ class LiveViewProxyAdapter(ApiAdapter):
             self.source_endpoints = []
             for endpoint in self.options.get(SOURCE_ENDPOINTS_CONFIG_NAME, "").split(','):
                 self.source_endpoints.append(endpoint.strip())
-                
+
+        if self.options.get(DEST_ENDPOINT_CONFIG_NAME, False):
+            self.dest_endpoint = self.options.get(DEST_ENDPOINT_CONFIG_NAME, "").strip()
+
+        self.param_tree = ParameterTree({
+            "source_endpoints": (self.source_endpoints, None),
+            "target_endpoint": (self.dest_endpoint, None)
+        }) 
 
     @response_types('application/json', 'image/*', default='application/json')
     def get(self, path, request):
-        pass
+        response = self.param_tree.get(path)
+        content_type = 'application/json'
+        status = 200
+        return ApiAdapterResponse(response, content_type, status)
 
     @response_types('application/json', default='application/json')
     def put(self, path, request):
-        pass
+        response = "PUT method not implemented by {}".format(self.name)
+        return ApiAdapterResponse(response, status_code=400)
 
     def cleanup(self):
         pass
 
 
 class LiveViewProxy(object):
+    """
 
+    """
     def __init__(self, source_endpoints, target_endpoint):
-        pass
+        logging.debug("Live View Proxy Initialising")
 
+        self.sub_channels = []
+        for endpoint in source_endpoints:
+            tmp_channel = IpcTornadoChannel(IpcTornadoChannel.CHANNEL_TYPE_SUB, endpoint=endpoint)
+            tmp_channel.subscribe()
+            tmp_channel.connect()
+            tmp_channel.register_callback(self.pub_if_newer)
+            self.sub_channels.append(tmp_channel)
+
+        self.pub_channel = IpcTornadoChannel(
+            IpcTornadoChannel.CHANNEL_TYPE_PUB,
+            endpoint=target_endpoint)
+
+    def pub_if_newer(self, msg):
+        """
+
+        """
+        # TODO: compare when frame was sent, if older than most recent shown one discard it
+        self.pub_channel.send(msg)
+        
