76c4efbd61a8636c3b9cf34df7fe9ba9a9e4024a
Adam Neaves <adam.neaves@stfc.ac.uk>
2019-02-11 11:44:17 +0000

    Added docstring. Added reset to param tree. PUTing to reset resets the statistics on dropped frames and the last frame sent.

--- tools/python/odin_data/live_view_proxy_adapter.py
+++ tools/python/odin_data/live_view_proxy_adapter.py
@@ -29,7 +29,7 @@ SOURCE_ENDPOINTS_CONFIG_NAME = 'source_endpoints'
 DEST_ENDPOINT_CONFIG_NAME = 'destination_endpoint'
 QUEUE_LENGTH_CONFIG_NAME = 'queue_length'
 
-DEFAULT_SOURCE_ENDPOINTS = ["tcp://127.0.0.1:5010"]
+DEFAULT_SOURCE_ENDPOINTS = "tcp://127.0.0.1:5010"
 DEFAULT_DEST_ENDPOINT = "tcp://127.0.0.1:5020"
 DEFAULT_QUEUE_LENGTH = 10
 
@@ -52,31 +52,34 @@ class LiveViewProxyAdapter(ApiAdapter):
         self.dest_endpoint = self.options.get(DEST_ENDPOINT_CONFIG_NAME,
                                               DEFAULT_DEST_ENDPOINT).strip()
 
-        logging.debug("Connecting publish socket to endpoint: %s", self.dest_endpoint)
-        self.publish_channel = IpcTornadoChannel(IpcTornadoChannel.CHANNEL_TYPE_PUB,
-                                                 self.dest_endpoint)
-        self.publish_channel.bind()
-
+        try:
+            logging.debug("Connecting publish socket to endpoint: %s", self.dest_endpoint)
+            self.publish_channel = IpcTornadoChannel(IpcTornadoChannel.CHANNEL_TYPE_PUB,
+                                                     self.dest_endpoint)
+            self.publish_channel.bind()
+        except IpcChannelException as channel_err:
+            logging.error("Connection Failed. Error given: %s", channel_err.message)
         self.max_queue = self.options.get(QUEUE_LENGTH_CONFIG_NAME, DEFAULT_QUEUE_LENGTH)
 
-        if self.options.get(SOURCE_ENDPOINTS_CONFIG_NAME, False):
+        if SOURCE_ENDPOINTS_CONFIG_NAME in self.options:
             self.source_endpoints = []
-            for target_str in self.options.get(SOURCE_ENDPOINTS_CONFIG_NAME).split(','):
+            for target_str in self.options[SOURCE_ENDPOINTS_CONFIG_NAME].split(','):
                 try:
                     (target, url) = target_str.strip().split("=")
                     self.source_endpoints.append(LiveViewProxyNode(target, url, self.add_to_queue))
                 except ValueError:
                     logging.debug("Error parsing target list: %s", target_str)
         else:
-            self.source_endpoints = LiveViewProxyNode(
+            self.source_endpoints = [LiveViewProxyNode(
                 "node_1",
                 DEFAULT_SOURCE_ENDPOINTS,
-                self.add_to_queue)
+                self.add_to_queue)]
 
         tree = {
             "target_endpoint": (self.get_target_endpoint, None),
             'last_sent_frame': (self.get_last_frame, None),
             'dropped_frames': (self.get_dropped_frames, None),
+            'reset': (None, self.set_reset),
             "nodes": {}
         }
         for sub in self.source_endpoints:
@@ -89,12 +92,11 @@ class LiveViewProxyAdapter(ApiAdapter):
         self.last_sent_frame = 0
         self.dropped_frame_count = 0
 
-        self.get_frame_from_queue(1)
+        self.get_frame_from_queue(0)
 
     @run_on_executor
     def get_frame_from_queue(self, task_interval):
-        """Run the adapter background task.
-
+        """
         loop to pop frames off the queue and send them to the destination ZMQ socket
         """
         try:
@@ -105,7 +107,8 @@ class LiveViewProxyAdapter(ApiAdapter):
         except QueueEmptyException:
             # queue is empty but thats fine, no need to report
             # or there'd be far too much output
-            logging.debug("Queue of frames empty. Nothing to send")
+            # logging.debug("Queue of frames empty. Nothing to send")
+            pass
         finally:
             time.sleep(task_interval)
             IOLoop.instance().add_callback(self.get_frame_from_queue, task_interval)
@@ -119,13 +122,25 @@ class LiveViewProxyAdapter(ApiAdapter):
 
     @response_types('application/json', default='application/json')
     def put(self, path, request):
-        response = "PUT method not implemented by {}".format(self.name)
-        return ApiAdapterResponse(response, status_code=400)
+        try:
+            data = json_decode(request.body)
+            self.param_tree.set(path, data)
+            response = self.param_tree.get(path)
+            status_code = 200
+        except ParameterTreeError as set_err:
+            response = {'error': str(set_err)}
+            status_code = 400
+        return ApiAdapterResponse(response, status_code=status_code)
 
     def cleanup(self):
         pass
 
     def add_to_queue(self, frame, source):
+        """
+        Add the frame to the priority queue, so long as it's "new enough" (the frame number is
+        not lower than the last sent frame)
+        If the queue is full, the next frame should be removed and this frame added instead.
+        """
         logging.debug("Add to queue called from %s", source.get_name())
         logging.debug("Adding frame number: %d", frame.get_number())
         if frame.get_number() < self.last_sent_frame:
@@ -141,13 +156,35 @@ class LiveViewProxyAdapter(ApiAdapter):
             self.dropped_frame_count += 1
 
     def get_last_frame(self):
+        """
+        return the frame number of the last sent frame
+        """
         return self.last_sent_frame
 
     def get_target_endpoint(self):
+        """
+        return the endpoint address of the target endpoint
+        """
         return self.dest_endpoint
 
     def get_dropped_frames(self):
+        """
+        return the number of frames dropped due to the queue being full
+        """
         return self.dropped_frame_count
+
+    def set_reset(self, data):
+        """
+        reset the statistics for a new aquisition, setting dropped frames and sent frame
+        counters back to 0
+        """
+        # TODO: find way to stop queue accepting new frames while resetting
+        while not self.queue.empty():
+            pass  # wait for queue to empty before resetting
+        self.last_sent_frame = 0
+        self.dropped_frame_count = 0
+        for node in self.source_endpoints:
+            node.set_reset()
 
 
 class LiveViewProxyNode(object):
@@ -178,28 +215,50 @@ class LiveViewProxyNode(object):
 
     def local_callback(self, msg):
         """
-        Creates an instance of the Frame class using the data from the socket.
-        Then, passed the frame on to the adapter to add to the Priority Queue.
-        If the priority queue is full, it will throw an exception and this frame will be discarded
+        Create an instance of the Frame class using the data from the socket.
+        Then, pass the frame on to the adapter to add to the Priority Queue.
         """
         tmp_frame = Frame(msg)
         self.received_frame_count += 1
         self.callback(tmp_frame, self)
 
     def get_endpoint(self):
+        """
+        return the endpoint address of the source socket
+        """
         return self.endpoint
 
     def get_dropped_frames(self):
+        """
+        return the number of frames dropped due to them being older than the last sent frame
+        """
         return self.dropped_frame_count
 
     def get_received_frames(self):
+        """
+        return the number of frames received from the source socket
+        """
         return self.received_frame_count
 
     def get_name(self):
+        """
+        return the node name
+        """
         return self.name
 
     def dropped_frame(self):
+        """
+        increase the count of dropped frames due to frame age
+        """
         self.dropped_frame_count += 1
+
+    def set_reset(self):
+        """
+        reset frame counts for dropped and received frames for starting
+        a new aqquisition
+        """
+        self.dropped_frame_count = 0
+        self.received_frame_count = 0
 
 
 class Frame(object):
@@ -215,10 +274,19 @@ class Frame(object):
         return self.num < other.num
 
     def get_header(self):
+        """
+        Return the frame header as a json encoded string
+        """
         return json_encode(self.header)
 
     def get_number(self):
+        """
+        Return the frame number
+        """
         return self.num
 
     def get_data(self):
+        """
+        Return the data of the frame
+        """
         return self.data
