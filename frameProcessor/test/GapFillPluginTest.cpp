0523966e3112bcf35ae2fc172b7843f153b31876
Alan Greer <alan.greer@diamond.ac.uk>
2019-02-15 10:34:11 +0000

    Added gap fill plugin checks for different sized chips, and to test some bad configurations.

--- frameProcessor/test/GapFillPluginTest.cpp
+++ frameProcessor/test/GapFillPluginTest.cpp
@@ -31,11 +31,28 @@ public:
         frame->set_data_type(1); // 0: UINT8, 1: UINT16, 2: UINT32
         frame->set_acquisition_id("scan1");
 
+        unsigned short img_2[16] = {1, 1, 2, 2,
+                                    1, 1, 2, 2,
+                                    3, 3, 4, 4,
+                                    3, 3, 4, 4};
+        img_dims[0] = 4;
+        img_dims[1] = 4;
+        chunk_dims[0] = 1;
+        chunk_dims[1] = 4;
+        chunk_dims[2] = 4;
+
+        frame_2 = boost::shared_ptr<FrameProcessor::Frame>(new FrameProcessor::Frame("data"));
+        frame_2->set_frame_number(7);
+        frame_2->set_dimensions(img_dims);
+        frame_2->copy_data(static_cast<void *>(img_2), 32);
+        frame_2->set_data_type(1); // 0: UINT8, 1: UINT16, 2: UINT32
+        frame_2->set_acquisition_id("scan1");
     }
 
     ~GapFillPluginTestFixture() {}
 
     boost::shared_ptr <FrameProcessor::Frame> frame;
+    boost::shared_ptr <FrameProcessor::Frame> frame_2;
     FrameProcessor::GapFillPlugin gap_fill_plugin;
 };
 
@@ -43,53 +60,137 @@ BOOST_FIXTURE_TEST_SUITE(GapFillPluginUnitTest, GapFillPluginTestFixture
 );
 
 BOOST_AUTO_TEST_CASE( GapFillPlugin_process_frame )
-        {
-                // Setup the plugin ready to insert gaps in both x and y direction
-                OdinData::IpcMessage reply;
-        OdinData::IpcMessage cfg;
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 3));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 4));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 3));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
-        BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+{
+    // Test a chip and grid config that will fail with the incoming frame
+    OdinData::IpcMessage reply_bad_cfg_1;
+    OdinData::IpcMessage bad_cfg_1;
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 5));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 4));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 3));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_1.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
 
-        gap_fill_plugin.configure(cfg, reply);
+    gap_fill_plugin.configure(bad_cfg_1, reply_bad_cfg_1);
 
-        // Push the frame through the plugin to force the gap fill
-        boost::shared_ptr<FrameProcessor::Frame> gap_frame = gap_fill_plugin.insert_gaps(frame);
+    // The following call should produce no return frame
+    boost::shared_ptr<FrameProcessor::Frame> bad_frame_1 = gap_fill_plugin.insert_gaps(frame);
+    BOOST_CHECK(!bad_frame_1);
 
-        unsigned short gap_img[117] = {
+    // Test an inconsistent gap array compared with the grid config
+    OdinData::IpcMessage reply_bad_cfg_2;
+    OdinData::IpcMessage bad_cfg_2;
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 3));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 4));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(bad_cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+
+    gap_fill_plugin.configure(bad_cfg_2, reply_bad_cfg_2);
+
+    // The following call should produce no return frame
+    boost::shared_ptr<FrameProcessor::Frame> bad_frame_2 = gap_fill_plugin.insert_gaps(frame);
+    BOOST_CHECK(!bad_frame_2);
+
+    // Setup the plugin ready to insert gaps in both x and y direction
+    OdinData::IpcMessage reply;
+    OdinData::IpcMessage cfg;
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 3));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 4));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 3));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+
+    gap_fill_plugin.configure(cfg, reply);
+
+    // Push the frame through the plugin to force the gap fill
+    boost::shared_ptr<FrameProcessor::Frame> gap_frame = gap_fill_plugin.insert_gaps(frame);
+
+    unsigned short gap_img[117] = {
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 1, 0, 0, 2, 0, 0, 0, 3, 0, 0, 4, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 5, 0, 0, 6, 0, 0, 0, 7, 0, 0, 8, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                    0, 9, 0, 0, 10, 0, 0, 0, 11, 0, 0, 12, 0,
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
-        };
+            0, 1, 0, 0, 2, 0, 0, 0, 3, 0, 0, 4, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 5, 0, 0, 6, 0, 0, 0, 7, 0, 0, 8, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 9, 0, 0, 10, 0, 0, 0, 11, 0, 0, 12, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+    };
 
-        // Verify the resultant frame has the correct dimensions and the gaps have been inserted correctly
-        unsigned short *ptr;
-        BOOST_REQUIRE_NO_THROW(ptr = (unsigned short *)gap_frame->get_data());
-        BOOST_CHECK_EQUAL(gap_frame->get_dimensions()[0], 9);
-        BOOST_CHECK_EQUAL(gap_frame->get_dimensions()[1], 13);
-        int index = 0;
-        for (int y = 0; y < gap_frame->get_dimensions()[0]; y++){
-            for (int x = 0; x < gap_frame->get_dimensions()[1]; x++) {
-                BOOST_CHECK_EQUAL(ptr[index], gap_img[index]);
-                index++;
-            }
+    // Verify the resultant frame has the correct dimensions and the gaps have been inserted correctly
+    unsigned short *ptr;
+    BOOST_REQUIRE_NO_THROW(ptr = (unsigned short *)gap_frame->get_data());
+    BOOST_CHECK_EQUAL(gap_frame->get_dimensions()[0], 9);
+    BOOST_CHECK_EQUAL(gap_frame->get_dimensions()[1], 13);
+    int index = 0;
+    for (int y = 0; y < gap_frame->get_dimensions()[0]; y++){
+        for (int x = 0; x < gap_frame->get_dimensions()[1]; x++) {
+            BOOST_CHECK_EQUAL(ptr[index], gap_img[index]);
+            index++;
         }
-        };
+    }
+
+    // Now test with a chip size of 2x2 and gaps of 2 in both directions
+    OdinData::IpcMessage reply_2;
+    OdinData::IpcMessage cfg_2;
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_CHIP_SIZE + "[]", 2));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_X_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+    BOOST_REQUIRE_NO_THROW(cfg_2.set_param(FrameProcessor::GapFillPlugin::CONFIG_GRID_Y_GAPS + "[]", 1));
+
+    gap_fill_plugin.configure(cfg_2, reply_2);
+
+    // Push the frame through the plugin to force the gap fill
+    boost::shared_ptr<FrameProcessor::Frame> gap_frame_2 = gap_fill_plugin.insert_gaps(frame_2);
+
+    unsigned short gap_img_2[49] = {
+            0, 0, 0, 0, 0, 0, 0,
+            0, 1, 1, 0, 2, 2, 0,
+            0, 1, 1, 0, 2, 2, 0,
+            0, 0, 0, 0, 0, 0, 0,
+            0, 3, 3, 0, 4, 4, 0,
+            0, 3, 3, 0, 4, 4, 0,
+            0, 0, 0, 0, 0, 0, 0
+    };
+
+
+    // Verify the resultant frame has the correct dimensions and the gaps have been inserted correctly
+    BOOST_REQUIRE_NO_THROW(ptr = (unsigned short *)gap_frame_2->get_data());
+    BOOST_CHECK_EQUAL(gap_frame_2->get_dimensions()[0], 7);
+    BOOST_CHECK_EQUAL(gap_frame_2->get_dimensions()[1], 7);
+    index = 0;
+    for (int y = 0; y < gap_frame_2->get_dimensions()[0]; y++){
+        for (int x = 0; x < gap_frame_2->get_dimensions()[1]; x++) {
+            BOOST_CHECK_EQUAL(ptr[index], gap_img_2[index]);
+            index++;
+        }
+    }
+
+};
 
 BOOST_AUTO_TEST_SUITE_END(); //GapFillPluginUnitTest
