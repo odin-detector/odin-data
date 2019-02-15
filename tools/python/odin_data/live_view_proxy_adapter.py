942b628374d6a746ca88b5bfe9588744212ee3d3
Adam Neaves <adam.neaves@stfc.ac.uk>
2019-02-15 14:47:22 +0000

    Added warning when percent of frames received by a node that are dropped goes above a certain threshold. Currently presents warning every time new acquisition starts but might need smarter method to keep warning visible

--- tools/python/odin_data/live_view_proxy_adapter.py
+++ tools/python/odin_data/live_view_proxy_adapter.py
@@ -13,11 +13,8 @@ from queue import PriorityQueue
 from queue import Full as QueueFullException
 from queue import Empty as QueueEmptyException
 
-import time
-from concurrent import futures
 from tornado.escape import json_decode, json_encode
 from tornado.ioloop import IOLoop
-from tornado.concurrent import run_on_executor
 
 from odin_data.ipc_tornado_channel import IpcTornadoChannel
 from odin_data.ipc_channel import IpcChannelException
@@ -28,10 +25,12 @@ from odin.adapters.parameter_tree import ParameterTree, ParameterTreeError
 SOURCE_ENDPOINTS_CONFIG_NAME = 'source_endpoints'
 DEST_ENDPOINT_CONFIG_NAME = 'destination_endpoint'
 QUEUE_LENGTH_CONFIG_NAME = 'queue_length'
+DROP_WARN_CONFIG_NAME = 'dropped_frame_warning_cutoff'
 
-DEFAULT_SOURCE_ENDPOINTS = "tcp://127.0.0.1:5010"
+DEFAULT_SOURCE_ENDPOINT = "tcp://127.0.0.1:5010"
 DEFAULT_DEST_ENDPOINT = "tcp://127.0.0.1:5020"
 DEFAULT_QUEUE_LENGTH = 10
+DEFAULT_DROP_WARN_PERCENT = 0.5
 
 
 class LiveViewProxyAdapter(ApiAdapter):
@@ -41,9 +40,6 @@ class LiveViewProxyAdapter(ApiAdapter):
     Implements the live view proxy adapter for odin control
     """
 
-    # Thread executor used for background tasks
-    executor = futures.ThreadPoolExecutor(max_workers=1)
-
     def __init__(self, **kwargs):
 
         logging.debug("Live View Proxy Adapter init called")
@@ -51,6 +47,9 @@ class LiveViewProxyAdapter(ApiAdapter):
 
         self.dest_endpoint = self.options.get(DEST_ENDPOINT_CONFIG_NAME,
                                               DEFAULT_DEST_ENDPOINT).strip()
+
+        self.drop_warn_percent = float(self.options.get(DROP_WARN_CONFIG_NAME,
+                                                        DEFAULT_DROP_WARN_PERCENT))
 
         try:
             logging.debug("Connecting publish socket to endpoint: %s", self.dest_endpoint)
@@ -65,14 +64,19 @@ class LiveViewProxyAdapter(ApiAdapter):
             self.source_endpoints = []
             for target_str in self.options[SOURCE_ENDPOINTS_CONFIG_NAME].split(','):
                 try:
-                    (target, url) = target_str.strip().split("=")
-                    self.source_endpoints.append(LiveViewProxyNode(target, url, self.add_to_queue))
+                    (target, url) = target_str.split("=")
+                    self.source_endpoints.append(LiveViewProxyNode(
+                        target.strip(),
+                        url.strip(),
+                        self.drop_warn_percent,
+                        self.add_to_queue))
                 except ValueError:
                     logging.debug("Error parsing target list: %s", target_str)
         else:
             self.source_endpoints = [LiveViewProxyNode(
                 "node_1",
-                DEFAULT_SOURCE_ENDPOINTS,
+                DEFAULT_SOURCE_ENDPOINT,
+                self.drop_warn_percent,
                 self.add_to_queue)]
 
         tree = {
@@ -92,16 +96,15 @@ class LiveViewProxyAdapter(ApiAdapter):
         self.last_sent_frame = (0, 0)
         self.dropped_frame_count = 0
 
-        self.get_frame_from_queue(0)
+        self.get_frame_from_queue()
 
-    @run_on_executor
-    def get_frame_from_queue(self, task_interval):
+    def get_frame_from_queue(self):
         """
         loop to pop frames off the queue and send them to the destination ZMQ socket
         """
         try:
             frame = self.queue.get_nowait()
-            logging.debug("Got frame %d:%d from queue", frame.acq_id, frame.get_number())
+            # logging.debug("Got frame %d:%d from queue", frame.acq_id, frame.get_number())
             self.last_sent_frame = (frame.get_acq(), frame.get_number())
             self.publish_channel.send_multipart([frame.get_header(), frame.get_data()])
         except QueueEmptyException:
@@ -109,10 +112,9 @@ class LiveViewProxyAdapter(ApiAdapter):
             # or there'd be far too much output
             pass
         finally:
-            time.sleep(task_interval)
-            IOLoop.instance().add_callback(self.get_frame_from_queue, task_interval)
+            IOLoop.instance().call_later(0, self.get_frame_from_queue)
 
-    @response_types('application/json', 'image/*', default='application/json')
+    @response_types('application/json', default='application/json')
     def get(self, path, request):
         response = self.param_tree.get(path)
         content_type = 'application/json'
@@ -140,10 +142,7 @@ class LiveViewProxyAdapter(ApiAdapter):
         not lower than the last sent frame)
         If the queue is full, the next frame should be removed and this frame added instead.
         """
-        logging.debug("Add to queue called from %s", source.get_name())
-        logging.debug("Adding frame number: %d", frame.get_number())
         if (frame.get_acq(), frame.get_number()) < self.last_sent_frame:
-            logging.debug("Frame too old, disgarding")
             source.dropped_frame()
             return
         try:
@@ -191,8 +190,8 @@ class LiveViewProxyNode(object):
     Live View Proxy. Connect to a ZMQ socket from an Odin Data Live View Plugin, saving any frames
     that arrive in a queue and passing them on to the central proxy controller when needed.
     """
-    def __init__(self, name, endpoint, callback):
-        logging.debug("Live View Proxy Initialising: %s", endpoint)
+    def __init__(self, name, endpoint, drop_warn_cutoff, callback):
+        logging.debug("Live View Proxy Node Initialising: %s", endpoint)
         self.name = name
         self.endpoint = endpoint
         self.callback = callback
@@ -200,6 +199,9 @@ class LiveViewProxyNode(object):
         self.received_frame_count = 0
         self.last_frame = 0
         self.current_acq = 0
+        self.drop_warn_cutoff = drop_warn_cutoff
+        self.drop_unwarn_cutoff = drop_warn_cutoff * 0.75
+        self.has_warned = False
 
         self.channel = IpcTornadoChannel(IpcTornadoChannel.CHANNEL_TYPE_SUB, endpoint=endpoint)
         self.channel.subscribe()
@@ -211,7 +213,7 @@ class LiveViewProxyNode(object):
             'dropped_frames': (self.get_dropped_frames, None),
             'received_frames': (self.get_received_frames, None),
             'last_frame': (self.get_last_frame, None),
-            'current_acquisition': (self.get_current_acq, None)
+            'current_acquisition': (self.get_current_acq, None),
         })
 
         logging.debug("Proxy Connected to Socket: %s", endpoint)
@@ -225,10 +227,11 @@ class LiveViewProxyNode(object):
         self.received_frame_count += 1
         if tmp_frame.get_number() < self.last_frame:
             logging.debug(
-                "New Frame %d has lower number than last frame %d. Has a new acquisition started?",
-                tmp_frame.get_number(), self.last_frame
+                "Frame number has reset, new acquisition started on Node %s",
+                self.name
             )
             self.current_acq += 1
+            self.has_warned = False
 
         tmp_frame.set_acq(self.current_acq)
         self.last_frame = tmp_frame.get_number()
@@ -275,6 +278,15 @@ class LiveViewProxyNode(object):
         increase the count of dropped frames due to frame age
         """
         self.dropped_frame_count += 1
+        current_dropped_percent = float(self.dropped_frame_count) / float(self.received_frame_count)
+        if current_dropped_percent > self.drop_warn_cutoff and not self.has_warned:
+            logging.warning(
+                "Node %s has dropped %d%% of frames",
+                self.name,
+                current_dropped_percent*100)
+            self.has_warned = True
+        elif current_dropped_percent < self.drop_unwarn_cutoff:
+            self.has_warned = False
 
     def set_reset(self):
         """
@@ -285,6 +297,7 @@ class LiveViewProxyNode(object):
         self.received_frame_count = 0
         self.last_frame = 0
         self.current_acq = 0
+        self.has_warned = False
 
 
 class Frame(object):
@@ -319,7 +332,7 @@ class Frame(object):
         Return the data of the frame
         """
         return self.data
-    
+
     def get_acq(self):
         """
         Return the acquisition ID for the frame
