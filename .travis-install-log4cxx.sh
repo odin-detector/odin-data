6371886e73888cbe4aec8379a438bdb3171e5717
Sky French <sky.french@diamond.ac.uk>
2019-06-05 10:55:42 +0100

    fix typo in travis log4cxx install

--- .travis-install-log4cxx.sh
+++ .travis-install-log4cxx.sh
@@ -2,7 +2,7 @@
 VERSION="0.10.0"
 LOG4CXX="apache-log4cxx-${VERSION}"
 wget http://www.mirrorservice.org/sites/ftp.apache.org/logging/log4cxx/0.10.0/apache-log4cxx-${VERSION}.tar.gz
-tar xvfz ${$LOG4CXX}.tar.gz
+tar xvfz ${LOG4CXX}.tar.gz
 cd "$LOG4CXX"
 patch -p1 < ../.log4cxx.0.10.0.patch
 mkdir "$HOME/$LOG4CXX"
