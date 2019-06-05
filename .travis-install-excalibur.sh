b1e54f81736197187de76f79853096a1369c186e
Sky French <sky.french@diamond.ac.uk>
2019-06-05 10:47:15 +0100

    adding README; updates to travis configuration

---
+++ .travis-install-excalibur.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+VERSION="master"
+EXCALIBUR="excalibur-detector-${VERSION}"
+wget https://github.com/dls-controls/excalibur-detector/archive/master.tar.gz
+tar xvfz ${VERSION}.tar.gz
+cd "$EXCALIBUR"
+mkdir build;
+cd build;
+cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DBoost_NO_BOOST_CMAKE=ON -DCMAKE_INSTALL_PREFIX=$HOME/$EXCALIBUR -DLOG4CXX_ROOT_DIR=$HOME/$LOG4CXX -DODINDATA_ROOT_DIR=/scratch/percival/odin-data/prefix ../data;
+cmake --build . --target install;
