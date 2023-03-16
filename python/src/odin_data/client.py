89cfaa38146fbc3da60c8a91d847e99392bd0fef
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-03-16 14:27:55 +0000

    Fix isort and flake8 changes in client

--- python/src/odin_data/client.py
+++ python/src/odin_data/client.py
@@ -1,8 +1,15 @@
+"""Simple command-line client for interacting with and debugging odin-data applications.
+
+This module implements a simple command-line client that can be used to interact with and debug
+odin-data applications via their control interface.
+
+Tim Nicholls, STFC Detector Systems Software Group
+"""
 import argparse
-import logging
-import sys
-import os
 import json
+import logging
+import os
+import sys
 
 from odin_data.control.ipc_channel import IpcChannel
 from odin_data.control.ipc_message import IpcMessage
@@ -12,10 +19,15 @@ try:
 except ImportError:
     JSONDecodeError = ValueError
 
+
 class OdinDataClient(object):
+    """odin-data client.
+
+    This class implements the odin-data command-line client.
+    """
 
     def __init__(self):
-
+        """Initialise the odin-data client."""
         prog_name = os.path.basename(sys.argv[0])
 
         # Parse command line arguments
@@ -46,9 +58,10 @@ class OdinDataClient(object):
         self._run = True
 
     def _parse_arguments(self, prog_name=sys.argv[0]):
-
+        """Parse arguments from the command line."""
         parser = argparse.ArgumentParser(prog=prog_name, description='odin-data application client')
-        parser.add_argument('--ctrl', type=str, default='tcp://127.0.0.1:5000', dest='ctrl_endpoint',
+        parser.add_argument('--ctrl', type=str, default='tcp://127.0.0.1:5000',
+                            dest='ctrl_endpoint',
                             help='Specify the IPC control channel endpoint URL')
         parser.add_argument('--config', type=argparse.FileType('r'), dest='config_file', nargs='?',
                             default=None, const=sys.stdin,
@@ -64,12 +77,12 @@ class OdinDataClient(object):
         return args
 
     def _next_msg_id(self):
-
+        """Return the next IPC message ID to use."""
         self._msg_id += 1
         return (self._msg_id)
 
     def run(self):
-
+        """Run the odin-data client."""
         self.logger.info("odin-data client starting up")
 
         self.logger.debug("Control IPC channel has identity {}".format(self.ctrl_channel.identity))
@@ -87,7 +100,7 @@ class OdinDataClient(object):
             self.do_shutdown_cmd()
 
     def do_config_cmd(self, config_file):
-
+        """Send a configure command to odin-data with the specified JSON configuration file."""
         try:
             config_params = json.load(config_file)
 
@@ -105,28 +118,28 @@ class OdinDataClient(object):
             self.logger.error("Failed to parse configuration file: {}".format(e))
 
     def do_status_cmd(self):
-
+        """Send a status command to odin-data."""
         status_msg = IpcMessage('cmd', 'status', id=self._next_msg_id())
         self.logger.info("Sending status request to the odin-data application")
         self.ctrl_channel.send(status_msg.encode())
         self.await_response()
 
     def do_request_config_cmd(self):
-
+        """Send a request configurtion command to odin-data."""
         status_msg = IpcMessage('cmd', 'request_configuration', id=self._next_msg_id())
         self.logger.info("Sending configuration request to the odin-data application")
         self.ctrl_channel.send(status_msg.encode())
         self.await_response()
 
     def do_shutdown_cmd(self):
-
+        """Send a shutdown command to odin-data."""
         shutdown_msg = IpcMessage('cmd', 'shutdown', id=self._next_msg_id())
         self.logger.info("Sending shutdown command to the odin-data application")
         self.ctrl_channel.send(shutdown_msg.encode())
         self.await_response()
 
     def await_response(self, timeout_ms=1000):
-
+        """Await a response to a client command."""
         pollevts = self.ctrl_channel.poll(1000)
         if pollevts == IpcChannel.POLLIN:
             reply = IpcMessage(from_str=self.ctrl_channel.recv())
@@ -134,8 +147,10 @@ class OdinDataClient(object):
 
 
 def main():
-        app = OdinDataClient()
-        app.run()
+    """Run the odin-data client."""
+    app = OdinDataClient()
+    app.run()
+
 
 if __name__ == "__main__":
     main()
