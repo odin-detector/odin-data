ab2719751e69048b2a49104cc41cff28dc998104
GDYendell <GDYendell@users.noreply.github.com>
2023-09-27 16:21:27 +0000

    deploy: 7bb0040fd753c747cf7cd230d50e7fc6b99e13d6

--- meta-string-types/_static/documentation_options.js
+++ meta-string-types/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gdc4deab',
+    VERSION: '0+untagged.1.g7bb0040',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
