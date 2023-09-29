1bfb5c2b581f56cba80016515bde0b8ed6a47cb9
Tim Nicholls <tim.nicholls@stfc.ac.uk>
2023-09-29 16:03:20 +0100

    Migrate unit tests to pytest and update

--- python/tests/test_ipc_message.py
+++ python/tests/test_ipc_message.py
@@ -1,9 +1,8 @@
-from nose.tools import assert_equals, assert_raises, assert_true, assert_false,\
-    assert_equal, assert_not_equal, assert_regexp_matches
+import pytest
 from odin_data.control.ipc_message import IpcMessage, IpcMessageException
 
-def test_valid_ipc_msg_from_string():
 
+def test_valid_ipc_msg_from_string():
     # Instantiate a valid message from a JSON string
 
     json_str = """
@@ -24,44 +23,43 @@ def test_valid_ipc_msg_from_string():
     the_msg = IpcMessage(from_str=json_str)
 
     # Check the message is indeed valid
-    assert_true(the_msg.is_valid())
+    assert the_msg.is_valid()
 
     # Check that all attributes are as expected
-    assert_equals(the_msg.get_msg_type(), "cmd")
-    assert_equals(the_msg.get_msg_val(), "status")
-    assert_equals(the_msg.get_msg_timestamp(), "2015-01-27T15:26:01.123456")
-    assert_equals(the_msg.get_msg_id(), 322)
+    assert the_msg.get_msg_type() == "cmd"
+    assert the_msg.get_msg_val() == "status"
+    assert the_msg.get_msg_timestamp() == "2015-01-27T15:26:01.123456"
+    assert the_msg.get_msg_id() == 322
 
     # Check that all parameters are as expected
-    assert_equals(the_msg.get_param("paramInt"), 1234)
-    assert_equals(the_msg.get_param("paramStr"), "testParam")
-    assert_equals(the_msg.get_param("paramDouble"), 3.1415)
+    assert the_msg.get_param("paramInt") == 1234
+    assert the_msg.get_param("paramStr") == "testParam"
+    assert the_msg.get_param("paramDouble") == 3.1415
 
     # Check valid message throws an exception on missing parameter
-    with assert_raises(IpcMessageException) as cm:
-         missingParam = the_msg.get_param("missingParam")
-    ex = cm.exception
-    assert_equals(ex.msg, 'Missing parameter missingParam')
+    with pytest.raises(IpcMessageException) as excinfo:
+        _ = the_msg.get_param("missingParam")
+    assert "Missing parameter missingParam" in str(excinfo.value)
 
     # Check valid message can fall back to default value if parameter missing
     defaultParamValue = 90210
-    assert_equals(the_msg.get_param("missingParam", defaultParamValue), defaultParamValue)
+    assert the_msg.get_param("missingParam", defaultParamValue) == defaultParamValue
+
 
 def test_empty_ipc_msg_invalid():
-
     # Instantiate an empty message
     the_msg = IpcMessage()
 
     # Check that the message is not valid
-    assert_false(the_msg.is_valid())
+    assert not the_msg.is_valid()
+
 
 def test_filled_ipc_msg_valid():
-
     # Instantiate an empty Message
     the_msg = IpcMessage()
 
     # Check that empty message is not valid
-    assert_false(the_msg.is_valid())
+    assert not the_msg.is_valid()
 
     # Set the message type, Value and id
     msg_type = "cmd"
@@ -72,41 +70,41 @@ def test_filled_ipc_msg_valid():
     the_msg.set_msg_id(msg_id)
 
     # Check that the message is now valid
-    assert_true(the_msg.is_valid())
+    assert the_msg.is_valid()
+
 
 def test_create_modify_empty_msg_params():
-
     # Instantiate an empty message
     the_msg = IpcMessage()
 
     # Define and set some parameters
-    paramInt1 = 1234;
-    paramInt2 = 901201;
-    paramInt3 = 4567;
+    paramInt1 = 1234
+    paramInt2 = 901201
+    paramInt3 = 4567
     paramStr = "paramString"
 
-    the_msg.set_param('paramInt1', paramInt1)
-    the_msg.set_param('paramInt2', paramInt2)
-    the_msg.set_param('paramInt3', paramInt3)
-    the_msg.set_param('paramStr',  paramStr)
+    the_msg.set_param("paramInt1", paramInt1)
+    the_msg.set_param("paramInt2", paramInt2)
+    the_msg.set_param("paramInt3", paramInt3)
+    the_msg.set_param("paramStr", paramStr)
 
     # Read them back and check they have the correct value
-    assert_true(the_msg.get_param('paramInt1'), paramInt1)
-    assert_true(the_msg.get_param('paramInt2'), paramInt2)
-    assert_true(the_msg.get_param('paramInt3'), paramInt3)
-    assert_true(the_msg.get_param('paramStr'),  paramStr)
+    assert the_msg.get_param("paramInt1") == paramInt1
+    assert the_msg.get_param("paramInt2") == paramInt2
+    assert the_msg.get_param("paramInt3") == paramInt3
+    assert the_msg.get_param("paramStr") == paramStr
 
     # Modify several parameters and check they are still correct
-    paramInt2 = 228724;
-    the_msg.set_param('paramInt2', paramInt2)
+    paramInt2 = 228724
+    the_msg.set_param("paramInt2", paramInt2)
     paramStr = "another string"
     the_msg.set_param("paramStr", paramStr)
 
-    assert_true(the_msg.get_param('paramInt2'), paramInt2)
-    assert_true(the_msg.get_param('paramStr'), paramStr)
+    assert the_msg.get_param("paramInt2") == paramInt2
+    assert the_msg.get_param("paramStr") == paramStr
+
 
 def test_round_trip_from_empty_msg():
-
     # Instantiate an empty message
     the_msg = IpcMessage()
 
@@ -118,16 +116,16 @@ def test_round_trip_from_empty_msg():
     msg_id = 61616
     the_msg.set_msg_id(msg_id)
 
-     # Define and set some parameters
-    paramInt1 = 1234;
-    paramInt2 = 901201;
-    paramInt3 = 4567;
+    # Define and set some parameters
+    paramInt1 = 1234
+    paramInt2 = 901201
+    paramInt3 = 4567
     paramStr = "paramString"
 
-    the_msg.set_param('paramInt1', paramInt1)
-    the_msg.set_param('paramInt2', paramInt2)
-    the_msg.set_param('paramInt3', paramInt3)
-    the_msg.set_param('paramStr',  paramStr)
+    the_msg.set_param("paramInt1", paramInt1)
+    the_msg.set_param("paramInt2", paramInt2)
+    the_msg.set_param("paramInt3", paramInt3)
+    the_msg.set_param("paramStr", paramStr)
 
     # Retrieve the encoded version
     the_msg_encoded = the_msg.encode()
@@ -136,17 +134,17 @@ def test_round_trip_from_empty_msg():
     msg_from_encoded = IpcMessage(from_str=the_msg_encoded)
 
     # Validate the contents of all attributes and parameters of the new message
-    assert_equal(msg_from_encoded.get_msg_type(), msg_type)
-    assert_equal(msg_from_encoded.get_msg_val(),  msg_val)
-    assert_equal(msg_from_encoded.get_msg_timestamp(), the_msg.get_msg_timestamp())
-    assert_equal(msg_from_encoded.get_msg_id(), the_msg.get_msg_id())
-    assert_equal(msg_from_encoded.get_param('paramInt1'), paramInt1)
-    assert_equal(msg_from_encoded.get_param('paramInt2'), paramInt2)
-    assert_equal(msg_from_encoded.get_param('paramInt3'), paramInt3)
-    assert_equal(msg_from_encoded.get_param('paramStr'), paramStr)
+    assert msg_from_encoded.get_msg_type() == msg_type
+    assert msg_from_encoded.get_msg_val() == msg_val
+    assert msg_from_encoded.get_msg_timestamp() == the_msg.get_msg_timestamp()
+    assert msg_from_encoded.get_msg_id() == the_msg.get_msg_id()
+    assert msg_from_encoded.get_param("paramInt1") == paramInt1
+    assert msg_from_encoded.get_param("paramInt2") == paramInt2
+    assert msg_from_encoded.get_param("paramInt3") == paramInt3
+    assert msg_from_encoded.get_param("paramStr") == paramStr
+
 
 def test_round_trip_from_empty_msg_comparison():
-
     # Instantiate an empty message
     the_msg = IpcMessage()
 
@@ -158,16 +156,16 @@ def test_round_trip_from_empty_msg_comparison():
     msg_id = 61616
     the_msg.set_msg_id(msg_id)
 
-     # Define and set some parameters
-    paramInt1 = 1234;
-    paramInt2 = 901201;
-    paramInt3 = 4567;
+    # Define and set some parameters
+    paramInt1 = 1234
+    paramInt2 = 901201
+    paramInt3 = 4567
     paramStr = "paramString"
 
-    the_msg.set_param('paramInt1', paramInt1)
-    the_msg.set_param('paramInt2', paramInt2)
-    the_msg.set_param('paramInt3', paramInt3)
-    the_msg.set_param('paramStr',  paramStr)
+    the_msg.set_param("paramInt1", paramInt1)
+    the_msg.set_param("paramInt2", paramInt2)
+    the_msg.set_param("paramInt3", paramInt3)
+    the_msg.set_param("paramStr", paramStr)
 
     # Retrieve the encoded version
     the_msg_encoded = the_msg.encode()
@@ -176,12 +174,11 @@ def test_round_trip_from_empty_msg_comparison():
     msg_from_encoded = IpcMessage(from_str=the_msg_encoded)
 
     # Test that the comparison operators work correctly
-    assert_true(the_msg == msg_from_encoded)
-    assert_false(the_msg != msg_from_encoded)
+    assert the_msg == msg_from_encoded
+    assert not (the_msg != msg_from_encoded)
+
 
 def test_invalid_msg_from_string():
-
-    with assert_raises(IpcMessageException) as cm:
-        invalid_msg = IpcMessage(from_str="{\"wibble\" : \"wobble\" \"shouldnt be here\"}")
-    ex = cm.exception
-    assert_regexp_matches(ex.msg, "Illegal message JSON format*")
+    with pytest.raises(IpcMessageException) as excinfo:
+        _ = IpcMessage(from_str='{"wibble" : "wobble" "shouldnt be here"}')
+    assert "Illegal message JSON format" in str(excinfo.value)
--- python/tests/test_util.py
+++ python/tests/test_util.py
@@ -1,17 +1,14 @@
-import unittest
-
 from odin_data.util import remove_prefix, remove_suffix
 
 
-class UtilTest(unittest.TestCase):
-
+class TestUtils:
     def test_remove_prefix(self):
         test = "config/hdf/frames"
         expected = "hdf/frames"
 
         result = remove_prefix(test, "config/")
 
-        self.assertEqual(expected, result)
+        assert expected == result
 
     def test_remove_suffix(self):
         test = "hdf/frames/0"
@@ -19,4 +16,4 @@ class UtilTest(unittest.TestCase):
 
         result = remove_suffix(test, "/0")
 
-        self.assertEqual(expected, result)
+        assert expected == result
