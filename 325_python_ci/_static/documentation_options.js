362cdf2c10d6c81b9a2bd03c40d50bb8eabf5106
timcnicholls <timcnicholls@users.noreply.github.com>
2023-10-02 09:51:25 +0000

    deploy: 279a3ed01ecb7cb00c1a7ae00149f318d3e5e61b

--- 325_python_ci/_static/documentation_options.js
+++ 325_python_ci/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gf9e30ee',
+    VERSION: '0+untagged.1.g279a3ed',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
