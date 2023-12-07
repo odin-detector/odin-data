df7e75a9e6b985f5216017a81d9383d8c4bc92e0
GDYendell <GDYendell@users.noreply.github.com>
2023-12-07 13:49:50 +0000

    deploy: adf75ea6ca451c4a81264a4008aadf6378451b5f

--- hdf5-references/_static/documentation_options.js
+++ hdf5-references/_static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.ge465b08',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '0+untagged.1.gadf75ea',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
@@ -8,7 +7,7 @@ var DOCUMENTATION_OPTIONS = {
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: true,
+    NAVIGATION_WITH_KEYS: false,
     SHOW_SEARCH_SUMMARY: true,
     ENABLE_SEARCH_SHORTCUTS: true,
 };
