5ade5007ae2b85c59030911636c905ce0cbb6d5a
Ohisemega <famousalele@gmail.com>
2025-09-12 20:12:58 +0100

    Implement Metadata Request in FPController API
    Modify FrameProcessorController Methods to allow checking for metadata and appending it:
        Add config IpcMessage to provideStatus API : void provideStatus(IpcMessage& config, bool metadata);
        Add config IpcMessage to requestConfiguration API: void requestConfiguration(IpcMessage& config, bool metadata);
    Add metadata request methods to FrameProcessorPlugin class.
    Fixes #406
    
    NB: The FrameProcessorPlugin methods:
    void requestConfiguration(OdinData::IpcMessage& reply);
    void requestConfigurationMetadata(OdinData::IpcMessage& reply);
    Are NOT YET implemented in this commit!

--- cpp/frameProcessor/include/FrameProcessorPlugin.h
+++ cpp/frameProcessor/include/FrameProcessorPlugin.h
@@ -45,6 +45,8 @@ public:
   std::vector<std::string> get_warnings();
   virtual void configure(OdinData::IpcMessage& config, OdinData::IpcMessage& reply);
   virtual void requestConfiguration(OdinData::IpcMessage& reply);
+  void requestConfigurationMetadata(OdinData::IpcMessage& reply);
+  void requestStatusMetadata(OdinData::IpcMessage& reply);
   virtual void execute(const std::string& command, OdinData::IpcMessage& reply);
   virtual std::vector<std::string> requestCommands();
   virtual void status(OdinData::IpcMessage& status);
