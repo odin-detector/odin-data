a13f9a274c9e3eb738a79a00b84b05ec70e5df13
Ohisemega <famousalele@gmail.com>
2026-02-09 15:03:51 +0000

    Add param metadata to blosc plugin

--- cpp/frameProcessor/include/BloscPlugin.h
+++ cpp/frameProcessor/include/BloscPlugin.h
@@ -42,6 +42,12 @@ public:
   virtual ~BloscPlugin();
   boost::shared_ptr<Frame> compress_frame(boost::shared_ptr<Frame> frame);
 
+  /** Configuration constants */
+  static const std::string CONFIG_BLOSC_COMPRESSOR;
+  static const std::string CONFIG_BLOSC_THREADS;
+  static const std::string CONFIG_BLOSC_LEVEL;
+  static const std::string CONFIG_BLOSC_SHUFFLE;
+
 private:
   // Baseclass API to implement:
   void process_frame(boost::shared_ptr<Frame> frame);
@@ -72,14 +78,6 @@ private:
   /** Temporary buffer for compressed data */
   void * data_buffer_ptr_;
   size_t data_buffer_size_;
-
-private:
-  /** Configuration constants */
-  static const std::string CONFIG_BLOSC_COMPRESSOR;
-  static const std::string CONFIG_BLOSC_THREADS;
-  static const std::string CONFIG_BLOSC_LEVEL;
-  static const std::string CONFIG_BLOSC_SHUFFLE;
-
 };
 
 } /* namespace FrameProcessor */
