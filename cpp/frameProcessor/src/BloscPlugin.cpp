2311c0671e6a065e870595bd8330df990e5d0c6d
Ohisemega <famousalele@gmail.com>
2026-03-06 10:48:57 +0000

    Replace BloscPlugin 'Allowed Values' Entry
    Replace BloscPlugin 'allowed values' entry with min and max parameters.

--- cpp/frameProcessor/src/BloscPlugin.cpp
+++ cpp/frameProcessor/src/BloscPlugin.cpp
@@ -55,10 +55,10 @@ void create_cd_values(const BloscCompressionSettings& settings, std::vector<unsi
 BloscPlugin::BloscPlugin() :
 current_acquisition_(""), data_buffer_ptr_(NULL), data_buffer_size_(0)
 {
-  add_config_param_metadata(CONFIG_BLOSC_COMPRESSOR, PMDD::UINT_T, PMDA::READ_WRITE, {BLOSC_BLOSCLZ, BLOSC_LZ4, BLOSC_LZ4HC, BLOSC_SNAPPY, BLOSC_ZLIB, BLOSC_ZSTD});
+  add_config_param_metadata(CONFIG_BLOSC_COMPRESSOR, PMDD::UINT_T, PMDA::READ_WRITE, 0, 5);
   add_config_param_metadata(CONFIG_BLOSC_THREADS, PMDD::UINT_T, PMDA::READ_WRITE, 1, ParamMetadata::MAX_UNSET);
   add_config_param_metadata(CONFIG_BLOSC_LEVEL, PMDD::INT_T, PMDA::READ_WRITE, 0, 9);
-  add_config_param_metadata(CONFIG_BLOSC_SHUFFLE, PMDD::UINT_T, PMDA::READ_WRITE, {BLOSC_NOSHUFFLE, BLOSC_SHUFFLE, BLOSC_BITSHUFFLE});
+  add_config_param_metadata(CONFIG_BLOSC_SHUFFLE, PMDD::UINT_T, PMDA::READ_WRITE, 0, 2);
 
   this->commanded_compression_settings_.blosc_compressor = BLOSC_LZ4;
   this->commanded_compression_settings_.shuffle = BLOSC_BITSHUFFLE;
