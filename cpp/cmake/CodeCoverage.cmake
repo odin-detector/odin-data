82248afa4ca80ad3d8dd302c72fcbfc7fe9e80a6
Andrew Duller <andyd@afterthoughtsoftware.com>
2026-03-13 08:39:37 +0000

    #451: Allow code coverage on all code.

--- cpp/cmake/CodeCoverage.cmake
+++ cpp/cmake/CodeCoverage.cmake
@@ -1,8 +1,25 @@
+# Control of code coverage is via two switches
+# - CODE_COVERAGE_ALL == 1 : this switches coverage on for all compilations
+# - CODE_COVERAGE_TARGET == 1 : this switches coverage on only if add_code_coverage has been used
+
+function(setup_coverage)
+  # Check only one of the code coverage switches has been set
+  if(DEFINED CODE_COVERAGE_ALL AND DEFINED CODE_COVERAGE_TARGET)
+    message(FATAL_ERROR "Both CODE_COVERAGE_ALL and CODE_COVERAGE_TARGET have been defined")
+  endif()
+
+  if(DEFINED CODE_COVERAGE_ALL AND CODE_COVERAGE_ALL)
+    add_compile_options(-O0 -g3 --coverage)
+    add_link_options(-O0 -g3 --coverage)
+    message(STATUS "CODE_COVERAGE_ALL: Global code coverage switch on")
+  endif()
+endfunction()
+
 function(add_code_coverage _target)
   # Both CODE_COVERAGE being defined and set to 1 is required
-  if(DEFINED CODE_COVERAGE AND CODE_COVERAGE)
+  if(DEFINED CODE_COVERAGE_TARGET AND CODE_COVERAGE_TARGET)
     target_compile_options(${_target} PRIVATE -O0 -g3 --coverage)
     target_link_options(${_target} PRIVATE -O0 -g3 --coverage)
-    message(STATUS "Added code coverage for target ${_target}")
-  endif(DEFINED CODE_COVERAGE AND CODE_COVERAGE)
+    message(STATUS "CODE_COVERAGE_TARGET: Added code coverage for target ${_target}")
+  endif(DEFINED CODE_COVERAGE_TARGET AND CODE_COVERAGE_TARGET)
 endfunction()
