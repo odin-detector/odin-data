1acb29d0eaee705caf2e30ad5695997336cb203a
Alan Greer <alan.greer@diamond.ac.uk>
2026-03-30 16:37:40 +0100

    Testing code

--- cpp/frameProcessor/test/FileWriterPluginTest.cpp
+++ cpp/frameProcessor/test/FileWriterPluginTest.cpp
@@ -158,7 +158,7 @@ BOOST_AUTO_TEST_CASE(FileWriterPluginStatusSanityCheck)
     using FPFW = FrameProcessor::FileWriterPlugin;
     fwp.set_name("hdf_status");
     BOOST_REQUIRE_NO_THROW(fwp.status(reply));
-    std::string&& prefix = fwp.get_name() + '/';
+    std::string prefix = fwp.get_name() + '/';
     BOOST_CHECK(reply.has_param(prefix + FPFW::STATUS_WRITING));
     BOOST_CHECK(reply.has_param(prefix + FPFW::STATUS_FRAMES_MAX));
     BOOST_CHECK(reply.has_param(prefix + FPFW::STATUS_FRAMES_WRITTEN));
